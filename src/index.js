49efd4e8434c1ca248cfdd01ae0c9a3819da6564
Billy Bastardi <billy@yext.com>
2019-02-21 18:25:03 -0500

    add some comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+// NOTE(billy) 
+// Each package or subpackage could have an index.js which would define what their package would export
+//
+// For instance, the search package can be something like:
+//
+// '/search/index.js'
+//      export { default as ... } from './class'
+//  
+// And this file could just:
+//    export * from '/search/index';
+//
+// But, since this example doesn't have a lot of code or subpackages, 
+// we could just access the classes directly from here:
+
 export { default as Search } from './search/search';
 export { default as Analytics } from './analytics/analytics';
 export { default as Rendering } from './rendering/rendering';
