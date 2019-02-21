a38d5575c1d89c266afea287e6659a3f8f61a411
Billy Bastardi <billy@yext.com>
2019-02-21 18:19:55 -0500

    fix babel iife

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 const { series, src, dest, watch } = require('gulp')
 const rollup = require('rollup-stream');
+const babel = require('rollup-plugin-babel');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 function bundle() {
   return rollup({
       input: './japi.js',
-      name: 'JAPI',
       format: 'iife',
+      name: 'JAPI',
+      plugins: [
+        babel({
+          exclude: 'node_modules/**',
+          presets: ['@babel/env']
+        })
+      ],
       sourcemap: true
     })
     .pipe(source('japi.min.js'))
     .pipe(buffer())
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-//   .pipe(uglify())
-   .pipe(dest('dist'));
+    .pipe(uglify())
+    .pipe(dest('dist'));
 }
 
 function minify(cb) {
