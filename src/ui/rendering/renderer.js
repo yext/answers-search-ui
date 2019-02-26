f3df2900afdfe8e13c81ed93cd1110d202fbf547
Billy Bastardi <billy@yext.com>
2019-02-26 17:50:06 -0500

    rendering: fix tab indent

--- src/ui/rendering/renderer.js
+++ src/ui/rendering/renderer.js
@@ -2,12 +2,12 @@
  * Renderer is an abstract class that all Renderers should extend and implement
  */
 export default class Renderer {
-	/**
-	 * render is a core method for all renderers.
-	 * All implementations should override this class
-	 * @param {string} template
-	 * @param {object} data
-	 */
+  /**
+   * render is a core method for all renderers.
+   * All implementations should override this class
+   * @param {string} template
+   * @param {object} data
+   */
   render(template, data) {
     return template;
   }
