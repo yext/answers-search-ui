4fdd5c6d5b394ff915e4eb111caff1f0463b7f01
Billy Bastardi <billy@yext.com>
2019-04-23 11:39:18 -0400

    ui/navigation: always show all navigation links provided

--- src/ui/components/navigation/navigationcomponent.js
+++ src/ui/components/navigation/navigationcomponent.js
@@ -11,9 +11,10 @@ export default class NavigationComponent extends Component {
     this._tabOrder = [];
 
     this._tabs = {};
+
+    // Build out oru tabs configuration
     for (let i = 0; i < opts.tabs.length; i++) {
       let tab = opts.tabs[i];
-
       this._tabOrder.push(tab.configId);
       this._tabs[tab.configId] = {
         label: tab.label,
@@ -27,11 +28,22 @@ export default class NavigationComponent extends Component {
       this._tabOrder = data.tabOrder;
     }
 
+    // Since our tab configuration is local, we generate it
+    // based on the feedback from the server
     let tabs = [];
     for (let i = 0; i < this._tabOrder.length; i ++) {
       let tab = this._tabs[this._tabOrder[i]];
       if (tab !== undefined) {
         tabs.push(tab)
+      }
+    }
+
+    // Because the tab order from server only includes
+    // sections that have results, we need to manually add
+    // the other tabs to the end of the array
+    for (let configId in this._tabs) {
+      if (this._tabOrder.indexOf(configId) <= -1) {
+        tabs.push(this._tabs[configId]);
       }
     }
 
