f857ce25abd36ce2d87f8631f6321071eb1aa125
Billy Bastardi <billy@yext.com>
2019-04-29 14:09:49 -0400

    update size limit config

--- .size-limit.js
+++ .size-limit.js
@@ -2,5 +2,9 @@ module.exports = [
     {
         path: "dist/answers.min.js",
         limit: "9 KB"
+    },
+    {
+        path: "dist/answerstemplates.compiled.min.js",
+        limit: "50 KB"
     }
 ]
