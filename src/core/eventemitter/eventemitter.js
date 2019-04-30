dbf8cfde5f86d53d076050267cbce98b15456cd9
Billy Bastardi <billy@yext.com>
2019-04-30 16:00:11 -0400

    core/ui: add documentation to eventemitter and state class

--- src/core/eventemitter/eventemitter.js
+++ src/core/eventemitter/eventemitter.js
@@ -1,8 +1,23 @@
+/**
+ * EventEmitter is a base class for any object that wants to expose
+ * a pub/sub interface, for emitting messages and providing listeners.
+ */
 export default class EventEmitter {
   constructor() {
+    /**
+     * The subscribers of messages
+     * @type {Array.<Object>}
+     * @private
+     */
     this._listeners = {};
   }
 
+  /**
+   * on is the public interface for subscribing events that are emitted.
+   * @param {string} evt the event name to listen to
+   * @param {function} cb The callback to invoke when the {evt} is emitted
+   * @param {boolean} once Optional value which will only handle the message once
+   */
   on(evt, cb, once) {
     if (typeof cb !== 'function') {
       throw new Error('callback handler should be of type {function}');
@@ -21,15 +36,32 @@ export default class EventEmitter {
     return this;
   }
 
+  /**
+   * once is the public interface for subscribing events that are emitted.
+   * The handler will only be triggered once.
+   *
+   * @param {string} evt the event name to listen to
+   * @param {function} cb The callback to invoke when the {evt} is emitted
+   * @param {boolean} once Optional value which will only handle the message once
+   */
   once(evt, cb) {
     return this.on(evt, cb, true);
   }
 
+  /**
+   * off is the public interface for unsubscribing from an event
+   * @param {string} evt the event name to unsubscribe from
+   */
   off(evt) {
     delete this._listeners[evt];
     return this;
   }
 
+  /**
+   * emit is the public interface for broadcasting messages/events
+   * @param {string} evt the event name to publish from
+   * @param {Object} data the data to send along to the subscribers
+   */
   emit(evt, data) {
     let listeners = this._listeners[evt];
     if (listeners === undefined) {
--- src/ui/components/state.js
+++ src/ui/components/state.js
@@ -1,26 +1,52 @@
 import EventEmitter from '../../core/eventemitter/eventemitter';
 
+/**
+ * State contains the data for the component
+ * and exposes an {EventEmitter} interface so that external
+ * dependencies can listen/hook subscribe to messages/updates.
+ */
 export default class State extends EventEmitter {
   constructor(data) {
     super();
 
-    this._state = data;
+    /**
+     * The initial state of the component
+     * @type {Object}
+     * @private
+     */
+    this._state = data || {};
   }
 
-  init(prop, val) {
-    this._set(prop, val);
+  /**
+   * Set the initial state of the component.
+   * NOTE(billy): Does not fire an update message
+   */
+  init(prop, opt_val) {
+    this._set(prop, opt_val);
   }
 
-  set(prop, val) {
-    this._set(prop, val);
+  /**
+   * setter for the state
+   * @param prop {string|Object} The property to set
+   * @param opt_val Optional, if prop is a {string}, it will assign the value to that property
+   */
+  set(prop, opt_val) {
+    this._set(prop, opt_val);
     this.emit('update');
   }
 
-  _set(prop, val) {
-    if (val === undefined) {
+  /**
+   * setter for the state enables you to update a single property, or complete state
+   * depending on the arguments provided.
+   * @param prop {string|Object} The property to set
+   * @param opt_val If prop is a {string}, provide its value
+   * @private
+   */
+  _set(prop, opt_val) {
+    if (opt_val === undefined) {
       this._state = prop;
     } else {
-      this._state[prop] = val;
+      this._state[prop] = opt_val;
     }
   }
 
@@ -29,11 +55,16 @@ export default class State extends EventEmitter {
     this.emit('update');
   }
 
-  get(prop) {
-    if (prop === undefined) {
+  /**
+   * Retrieve a properties value from the state
+   * If no property provided, return the full state
+   * @param {string} opt_prop optional property to retrieve
+   */
+  get(opt_prop) {
+    if (opt_prop === undefined) {
       return this._state;
     }
-    return this._state[prop];
+    return this._state[opt_prop];
   }
 
   asJSON() {
