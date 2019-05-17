732f806ad6d5b9d9e443dce11c464cff98faa599
Billy Bastardi <billy@yext.com>
2019-05-17 12:20:23 -0400

    core/autocomplete: fix multi-match highlighting

--- src/core/search/autocompletedatatransformer.js
+++ src/core/search/autocompletedatatransformer.js
@@ -27,20 +27,9 @@ export default class AutoCompleteDataTransformer {
     let data = [];
     for (let i = 0; i < results.length; i ++) {
       let value = results[i].value,
-          highlightedValue = value,
           subStrings = results[i].matchedSubstrings;
 
-      for (let j = 0; j < subStrings.length; j ++) {
-        let start = Number(subStrings[j].offset),
-            end = start + subStrings[j].length;
-
-        highlightedValue = [value.slice(0, start), '<strong>', value.slice(start, end), '</strong>', value.slice(end)].join('');
-      }
-
-      data.push({
-        shortValue: value,
-        highlightedValue: highlightedValue,
-      });
+      data.push(AutoCompleteDataTransformer.highlight(value, subStrings));
     }
 
     return AutoCompleteDataTransformer.clean('autocomplete', {
@@ -48,6 +37,50 @@ export default class AutoCompleteDataTransformer {
         'results': data
       }]
     });
+  }
+
+  static highlight(value, subStrings) {
+    if (!subStrings || subStrings.length === 0) {
+      return {
+        shortValue: value,
+        highlightedValue: value
+      };
+    }
+
+    // Make sure our highlighted substrings are sorted
+    subStrings.sort((a, b) => {
+      if (a.offset < b.offset) {
+        return -1;
+      }
+
+      if (a.offset > b.offset) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    // Build our new value based on the highlights
+    let highlightedValue = '',
+        nextStart = 0;
+
+    for (let j = 0; j < subStrings.length; j ++) {
+      let start = Number(subStrings[j].offset),
+          end = start + subStrings[j].length;
+
+      highlightedValue += [value.slice(nextStart, start), '<strong>', value.slice(start, end), '</strong>'].join('');
+
+      if (j === subStrings.length - 1 && end < value.length) {
+        highlightedValue += value.slice(end);
+      }
+
+      nextStart = end;
+    }
+
+    return {
+      shortValue: value,
+      highlightedValue: highlightedValue,
+    };
   }
 
   static vertical(response, barKey) {
