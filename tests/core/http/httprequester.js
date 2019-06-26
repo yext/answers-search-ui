6604522dbd8ed1b1bbf80e94a1e8efbd635740a8
Julian Delerme <jdelerme@yext.com>
2019-06-26 16:54:05 -0400

    Add tests for autocomplete search and httprequestor

---
+++ tests/core/http/httprequester.js
@@ -0,0 +1,177 @@
+import HttpRequester from '../../../src/core/http/httprequester';
+
+const baseUrl = 'https://liveapi.yext.com/v2/accounts/me';
+
+describe('resquesting and responding', () => {
+  let requester;
+  const apiKey = 'abc123';
+  const input = 'test';
+  const answersKey = '123456';
+  const verticalKey = 'unittests';
+  const barKey = 'testing';
+  const data = { apiKey, input, answersKey, verticalKey, barKey };
+
+  beforeEach(() => {
+    requester = new HttpRequester();
+  });
+
+  describe('get', () => {
+    const expectedUrl = `${baseUrl}/answers/query?apiKey=${apiKey}&input=${input}&answersKey=${answersKey}&verticalKey=${verticalKey}&barKey=${barKey}`;
+
+    it('creates a proper GET request with params', () => {
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
+
+      requester.get(`${baseUrl}/answers/query`, data, {});
+      expect(global.fetch).toHaveBeenCalledWith(
+        expectedUrl,
+        { method: 'get', credentials: 'include' }
+      );
+    });
+
+    it('uses the provided options for the request', () => {
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
+
+      requester.get(`${baseUrl}/answers/query`, data, { credentials: 'omit', redirect: 'error' });
+      expect(global.fetch).toHaveBeenCalledWith(
+        expectedUrl,
+        { method: 'get', credentials: 'omit', redirect: 'error' }
+      );
+    });
+
+    it('returns the response in a promise', () => {
+      const expectedResponse = {
+        response: {
+          businessId: 1234,
+          sections: [
+            {
+              label: 'Watson',
+              results: [
+                {
+                  key: 'name',
+                  value: 'Bret Quiqley',
+                  filter: { name: { $eq: 'Bret Quiqley' } }
+                }
+              ]
+            }
+          ]
+        }
+      };
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(expectedResponse) }));
+
+      expect.assertions(1);
+      return requester.get(`${baseUrl}/answers/query`, data, {})
+        .then(r => r.json())
+        .then(d => expect(d).toBe(expectedResponse));
+    });
+  });
+
+  describe('post', () => {
+    const expectedUrl = `${baseUrl}/answers/query`;
+
+    it('creates a proper POST request with body', () => {
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
+
+      requester.post(`${baseUrl}/answers/query`, data, {});
+      expect(global.fetch).toHaveBeenCalledWith(
+        expectedUrl,
+        { method: 'post', credentials: 'include', body: JSON.stringify(data) }
+      );
+    });
+
+    it('uses the provided options for the request', () => {
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
+
+      requester.post(`${baseUrl}/answers/query`, data, { credentials: 'omit', redirect: 'error' });
+      expect(global.fetch).toHaveBeenCalledWith(
+        expectedUrl,
+        { method: 'post', credentials: 'omit', redirect: 'error', body: JSON.stringify(data) }
+      );
+    });
+
+    it('returns the response in a promise', () => {
+      const expectedResponse = {
+        response: {
+          businessId: 1234,
+          sections: [
+            {
+              label: 'Watson',
+              results: [
+                {
+                  key: 'name',
+                  value: 'Bret Quiqley',
+                  filter: { name: { $eq: 'Bret Quiqley' } }
+                }
+              ]
+            }
+          ]
+        }
+      };
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(expectedResponse) }));
+
+      expect.assertions(1);
+      return requester.post(`${baseUrl}/answers/query`, data, {})
+        .then(r => r.json())
+        .then(d => expect(d).toBe(expectedResponse));
+    });
+  });
+
+  describe('request', () => {
+    const expectedUrl = `${baseUrl}/answers/query`;
+
+    it('creates a request with the provided method and options', () => {
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
+
+      requester.request('put', `${baseUrl}/answers/query`, { body: JSON.stringify(data) });
+      expect(global.fetch).toHaveBeenCalledWith(
+        expectedUrl,
+        { method: 'put', credentials: 'include', body: JSON.stringify(data) }
+      );
+    });
+
+    it('returns the response in a promise', () => {
+      const expectedResponse = {
+        response: {
+          businessId: 1234,
+          sections: [
+            {
+              label: 'Watson',
+              results: [
+                {
+                  key: 'name',
+                  value: 'Bret Quiqley',
+                  filter: { name: { $eq: 'Bret Quiqley' } }
+                }
+              ]
+            }
+          ]
+        }
+      };
+      global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(expectedResponse) }));
+
+      expect.assertions(1);
+      return requester.request('update', `${baseUrl}/answers/query`, { body: JSON.stringify(data) })
+        .then(r => r.json())
+        .then(d => expect(d).toBe(expectedResponse));
+    });
+  });
+
+  describe('encodeParams', () => {
+    const expectedUrl = `${baseUrl}/answers/query?apiKey=${apiKey}&input=${input}&answersKey=${answersKey}&verticalKey=${verticalKey}&barKey=${barKey}`;
+
+    it('adds all params to the url', () => {
+      const url = requester.encodeParams(`${baseUrl}/answers/query`, data);
+      expect(url).toBe(expectedUrl);
+    });
+
+    it('properly escapes & and ? and =', () => {
+      const data = {
+        a: '&pizza',
+        b: 'who is jsharps?',
+        c: '1+2=3'
+      };
+
+      const url = requester.encodeParams(`${baseUrl}/answers/query`, data);
+      expect(url).toBe(`${baseUrl}/answers/query?a=%26pizza&b=who%20is%20jsharps%3F&c=1%2B2%3D3`);
+    });
+  });
+});
---
+++ tests/ui/components/search/autocomplete.js
@@ -0,0 +1,127 @@
+import AutoComplete from '../../../../src/core/search/autocomplete';
+import AutoCompleteData from '../../../../src/core/models/autocompletedata';
+import HttpRequester from '../../../../src/core/http/httprequester';
+jest.mock('../../../../src/core/http/httprequester');
+
+const baseUrl = 'https://liveapi.yext.com/v2/accounts/me';
+
+describe('querying and responding', () => {
+  const apiKey = 'abc123';
+  const answersKey = '123456';
+  const verticalKey = 'unittests';
+  const barKey = 'testing';
+  const input = 'test';
+  const version = 20190101;
+
+  const expectedResponse = {
+    response: {
+      businessId: 1234,
+      sections: [
+        {
+          label: 'Watson',
+          results: [
+            {
+              key: 'name',
+              value: 'Bret Quiqley',
+              filter: { name: { $eq: 'Bret Quiqley' } }
+            }
+          ]
+        }
+      ]
+    }
+  };
+
+  let autocomplete;
+  const mockedGet = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(expectedResponse) }));
+
+  beforeEach(() => {
+    mockedGet.mockClear();
+    HttpRequester.mockImplementation(() => {
+      return {
+        get: mockedGet
+      };
+    });
+    autocomplete = new AutoComplete({
+      apiKey,
+      answersKey
+    });
+  });
+
+  describe('queryFilter', () => {
+    const expectedUrl = `${baseUrl}/answers/filtersearch`;
+    const expectedData = {
+      answersKey,
+      api_key: apiKey,
+      experienceKey: verticalKey,
+      input,
+      inputKey: barKey,
+      v: version
+    };
+
+    it('calls the get method with the filter url', () => {
+      autocomplete.queryFilter('test', verticalKey, barKey);
+      expect(mockedGet).toHaveBeenCalledTimes(1);
+      expect(mockedGet).toHaveBeenCalledWith(expectedUrl, expectedData);
+    });
+
+    it('returns the right AutoCompleteData', () => {
+      expect.assertions(3);
+      return autocomplete.queryFilter('test', verticalKey, barKey).then(d => {
+        expect(d instanceof AutoCompleteData).toBeTruthy();
+        expect(d.sections).toHaveLength(1);
+        expect(d.sections[0].results).toHaveLength(1);
+      });
+    });
+  });
+
+  describe('queryVertical', () => {
+    const expectedUrl = `${baseUrl}/entities/autocomplete`;
+    const expectedData = {
+      api_key: apiKey,
+      barKey,
+      experienceKey: verticalKey,
+      input,
+      v: version
+    };
+
+    it('creates a proper GET request for vertical search', () => {
+      autocomplete.queryVertical('test', verticalKey, barKey);
+      expect(mockedGet).toHaveBeenCalledTimes(1);
+      expect(mockedGet).toHaveBeenCalledWith(expectedUrl, expectedData);
+    });
+
+    it('returns the right AutoCompleteData', () => {
+      expect.assertions(3);
+      return autocomplete.queryVertical('test', verticalKey, barKey).then(d => {
+        expect(d instanceof AutoCompleteData).toBeTruthy();
+        expect(d.sections).toHaveLength(1);
+        expect(d.sections[0].results).toHaveLength(1);
+      });
+    });
+  });
+
+  describe('queryUniversal', () => {
+    const expectedUrl = `${baseUrl}/answers/autocomplete`;
+    const expectedData = {
+      answersKey,
+      api_key: apiKey,
+      input,
+      v: version
+    };
+
+    it('creates a proper GET request for vertical search', () => {
+      autocomplete.queryUniversal('test');
+      expect(mockedGet).toHaveBeenCalledTimes(1);
+      expect(mockedGet).toHaveBeenCalledWith(expectedUrl, expectedData);
+    });
+
+    it('returns the right AutoCompleteData', () => {
+      expect.assertions(3);
+      return autocomplete.queryUniversal('test', verticalKey, barKey).then(d => {
+        expect(d instanceof AutoCompleteData).toBeTruthy();
+        expect(d.sections).toHaveLength(1);
+        expect(d.sections[0].results).toHaveLength(1);
+      });
+    });
+  });
+});
