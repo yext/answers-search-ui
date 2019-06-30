d483a5aa2d6bed7f40b1bf44dc8a65e7489536e9
Billy Bastardi <billy@yext.com>
2019-06-30 16:01:07 -0400

    core/storage: all storage keys should be referenced from enum for searchability

--- src/ui/components/search/filterpickercomponent.js
+++ src/ui/components/search/filterpickercomponent.js
@@ -2,7 +2,7 @@
 
 import Component from '../component';
 import DOM from '../../dom/dom';
-import * as StorageKeys from '../../../core/storage/storagekeys';
+import { StorageKeys } from '../../../core/storage/storagekeys';
 import Filter from '../../../core/models/filter';
 
 export default class FilterPickerComponent extends Component {
