b4d8ae769a0b0edc82001a6c6848f7e6d84a0160
Billy Bastardi <billy@yext.com>
2019-07-15 17:05:05 -0400

    Navigation should support query params as config  (#54)
    * ui/nav: start refactor to fix tab ordering persistence through url params

--- src/ui/components/navigation/navigationcomponent.js
+++ src/ui/components/navigation/navigationcomponent.js
@@ -1,6 +1,80 @@
 /** @module NavigationComponent */
 
 import Component from '../component';
+import { AnswersComponentError } from '../../../core/errors/errors';
+
+/**
+ * The Tab is a model that is used to power the Navigation tabs in the view.
+ * It's initialized through the configuration provided to the component.
+ */
+export class Tab {
+  constructor (config) {
+    /**
+     * The name of the tab that is exposed for the link
+     * @type {string}
+     */
+    this.label = config.label;
+    if (typeof this.label !== 'string') {
+      throw new AnswersComponentError('label is a required configuration option for tab.', 'NavigationComponent');
+    }
+
+    /**
+     * The complete URL, including the params
+     * @type {string}
+     */
+    this.url = config.url;
+    if (typeof this.url !== 'string') {
+      throw new AnswersComponentError('url is a required configuration option for tab.', 'NavigationComponent');
+    }
+
+    /**
+     * The serverside vertical config id that this is referenced to.
+     * By providing this, enables dynamic sorting based on results.
+     * @type {string}
+     */
+    this.configId = config.configId || null;
+
+    /**
+     * The base URL used for constructing the URL with params
+     * @type {string}
+     */
+    this.baseUrl = config.url;
+
+    /**
+     * Determines whether to show this tab first in the order
+     * @type {boolean}
+     */
+    this.isFirst = config.isFirst || false;
+
+    /**
+     * Determines whether or not to apply a special class to the
+     * markup to determine if it's an active tab
+     * @type {boolean}
+     */
+    this.isActive = config.isActive || false;
+  }
+
+  /**
+   * from will construct a map of configId to {Tab} from
+   * a configuration file
+   * @param {object} tabsConfig the configuration to use
+   */
+  static from (tabsConfig) {
+    let tabs = {};
+    // Parse the options and build out our tabs and
+    for (let i = 0; i < tabsConfig.length; i++) {
+      let tab = tabsConfig[i];
+      // For tabs without config ids, map their URL to the configID
+      // to avoid duplication of renders
+      if (tab.configId === undefined && tabs[tab.configId] === undefined) {
+        tab.configId = tab.url;
+      }
+
+      tabs[tab.configId] = new Tab(tab);
+    }
+    return tabs;
+  }
+}
 
 /**
  * NavigationComponent exposes an interface for building a dynamic
@@ -8,8 +82,8 @@ import Component from '../component';
  * @extends Component
  */
 export default class NavigationComponent extends Component {
-  constructor (opts = {}) {
-    super(opts);
+  constructor (config = {}) {
+    super(config);
 
     /**
      * The data storage key
@@ -20,64 +94,28 @@ export default class NavigationComponent extends Component {
     /**
      * The handlebars template to use
      * @type {string}
+     * @private
      */
     this._templateName = 'navigation/navigation';
 
     /**
-     * The order of the tabs, parsed from configuration.
-     * This gets updated based on the server results
-     * @type {string[]} The list of VS configIds
+     * Unordered map of each tab, keyed by VS configId
+     * @type {Object.<String, Object>}
+     * @private
      */
-    this._tabOrder = [];
+    this._tabs = Tab.from(config.tabs);
 
     /**
-     * The data container for each tab, keyed by VS configId
-     * @type {Object.<String, Object>}
+     * The order of the tabs, parsed from configuration or URL.
+     * This gets updated based on the server results
+     * @type {Array.<String>} The list of VS configIds
+     * @private
      */
-    this._tabs = {};
-
-    // Parse the options and build out our tabs and
-    // tabOrder from the settings provided.
-    for (let i = 0; i < opts.tabs.length; i++) {
-      let tab = opts.tabs[i];
-      // For tabs without config ids, map their URL to the configID
-      // to avoid duplication of renders
-      if (tab.configId === undefined && this._tabs[tab.configId] === undefined) {
-        tab.configId = tab.url;
-      }
-
-      // isFirst should always be the first element in the list
-      if (tab.isFirst) {
-        this._tabOrder.unshift(tab.configId);
-      } else {
-        this._tabOrder.push(tab.configId);
-      }
-
-      this._tabs[tab.configId] = {
-        label: tab.label,
-        configId: tab.configId,
-        url: tab.url,
-        baseUrl: tab.url,
-        isFirst: tab.isFirst === true,
-        isActive: tab.isActive === true
-      };
-    }
+    this._tabOrder = this.getDefaultTabOrder(config.tabs, this.getUrlParams());
   }
 
   static get type () {
     return 'Navigation';
-  }
-
-  getUrlParams () {
-    return new URLSearchParams(window.location.search.substring(1));
-  }
-
-  generateTabUrl (tab) {
-    let params = this.getUrlParams();
-    if (params.toString().length > 0) {
-      tab.url = tab.baseUrl + '?' + params.toString();
-    }
-    return tab;
   }
 
   /**
@@ -89,32 +127,17 @@ export default class NavigationComponent extends Component {
    */
   setState (data) {
     if (data.tabOrder !== undefined) {
-      this._tabOrder = data.tabOrder;
+      this._tabOrder = this.mergeTabOrder(data.tabOrder, this._tabOrder);
     }
 
-    // Since our tab configuration is local, we generate it
-    // based on the feedback from the server
+    // Since the tab ordering can change based on the server data
+    // we need to update each tabs URL to include the order as part of their params.
+    // This helps with persisting state across verticals.
     let tabs = [];
     for (let i = 0; i < this._tabOrder.length; i++) {
       let tab = this._tabs[this._tabOrder[i]];
       if (tab !== undefined) {
-        tabs.push(this.generateTabUrl(tab));
-      }
-    }
-
-    // Because the tab order from server only includes
-    // sections that have results, we need to manually add
-    // the other tabs to the end of the array
-    for (let configId in this._tabs) {
-      if (this._tabOrder.indexOf(configId) <= -1) {
-        let tab = this.generateTabUrl(this._tabs[configId]);
-
-        // isFirst should always be the first element in the list
-        if (tab.isFirst) {
-          tabs.unshift(tab);
-          continue;
-        }
-
+        tab.url = this.generateTabUrl(tab.baseUrl, this.getUrlParams());
         tabs.push(tab);
       }
     }
@@ -124,7 +147,82 @@ export default class NavigationComponent extends Component {
     });
   }
 
-  render (data) {
-    return super.render(data);
+  getUrlParams () {
+    return new URLSearchParams(window.location.search.substring(1));
+  }
+
+  /**
+   * getDefaultTabOrder will compute the initial tab ordering based
+   * on a combination of the configuration provided directly to the component
+   * and the url params.
+   * @param {Object[]} tabsConfig
+   * @param {UrlSearchParams}
+   */
+  getDefaultTabOrder (tabsConfig, urlParams) {
+    let tabOrder = [];
+
+    // Use the ordering from the URL as the primary configuration
+    // And then merge it with the local configuration, if provided.
+    if (urlParams && urlParams.has('tabOrder')) {
+      tabOrder = urlParams.get('tabOrder').split(',');
+    }
+
+    for (let i = 0; i < tabsConfig.length; i++) {
+      const tab = tabsConfig[i];
+      // Some tabs don't have configId, so we map it from URL
+      if (tab.configId === undefined) {
+        tab.configId = tab.url;
+      }
+
+      // Avoid duplicates if config was provided from URL
+      if (tabOrder.includes(tab.configId)) {
+        continue;
+      }
+
+      // isFirst should always be the first element in the list
+      if (tab.isFirst) {
+        tabOrder.unshift(tab.configId);
+      } else {
+        tabOrder.push(tab.configId);
+      }
+    }
+
+    return tabOrder;
+  }
+
+  /**
+   * mergeTabOrder merges two arrays into one
+   * by appending additional tabs to the end of the original array
+   * @param {string[]} tabOrder Tab order provided by the server
+   * @param {string[]} otherTabOrder Tab order provided by configuration
+   * @return {string[]}
+   */
+  mergeTabOrder (tabOrder, otherTabOrder) {
+    for (let i = 0; i < otherTabOrder.length; i++) {
+      const tabConfig = otherTabOrder[i];
+      if (tabOrder.includes(tabConfig)) {
+        continue;
+      }
+
+      // isFirst should be an override to dynamic tab ordering.
+      if (this._tabs[tabConfig] && this._tabs[tabConfig].isFirst) {
+        tabOrder.unshift(tabConfig);
+      } else {
+        tabOrder.push(tabConfig);
+      }
+    }
+
+    return tabOrder;
+  }
+
+  generateTabUrl (baseUrl, params) {
+    // We want to persist the params from the existing URL to the new
+    // URLS we create.
+    let tabUrl;
+    if (params.toString().length > 0) {
+      params.set('tabOrder', this._tabOrder);
+      tabUrl = baseUrl + '?' + params.toString();
+    }
+    return tabUrl;
   }
 }
---
+++ tests/ui/components/navigation/navigationcomponent.js
@@ -0,0 +1,192 @@
+import DOM from '../../../../src/ui/dom/dom';
+import NavigationComponent, { Tab } from '../../../../src/ui/components/navigation/navigationcomponent';
+
+// The DOM doesn't exist within components in the JEST environment,
+// so we have to provide it to our DOM API properly.
+DOM.setup(
+  document,
+  new DOMParser()
+);
+
+beforeEach(() => {
+  // Always reset the DOM before each component render test
+  let bodyEl = DOM.query('body');
+  DOM.empty(bodyEl);
+
+  // Create the container that our component will be injected into
+  DOM.append(bodyEl, DOM.createEl('div', { class: 'test-component' }));
+});
+
+describe('navigation component configuration', () => {
+  it('tab model supports basic configuration', () => {
+    const config = {
+      label: 'This is my label',
+      url: 'This is my url'
+    };
+
+    const expected = {
+      label: config.label,
+      url: config.url,
+      baseUrl: config.url,
+      configId: null,
+      isFirst: false,
+      isActive: false
+    };
+
+    const tab = new Tab(config);
+
+    expect(tab).toMatchObject(expected);
+  });
+
+  it('tab model supports advanced configuration', () => {
+    const config = {
+      label: 'This is my label',
+      url: 'This is my url',
+      configId: 'test',
+      isFirst: true,
+      isActive: true
+    };
+
+    const expected = {
+      label: config.label,
+      url: config.url,
+      baseUrl: config.url,
+      configId: config.configId,
+      isFirst: config.isFirst,
+      isActive: config.isActive
+    };
+
+    const tab = new Tab(config);
+
+    expect(tab).toMatchObject(expected);
+  });
+
+  it('multi tab configuration', () => {
+    const tab1Config = {
+      label: 'This is my label',
+      url: '/tab1/'
+    };
+
+    const tab2Config = {
+      label: 'This this is my second label',
+      url: '/tab2/'
+    };
+
+    const expected = {
+      '/tab1/': {
+        label: tab1Config.label,
+        url: tab1Config.url,
+        configId: '/tab1/',
+        isFirst: false,
+        isActive: false
+      },
+      '/tab2/': {
+        label: tab2Config.label,
+        url: tab2Config.url,
+        configId: '/tab2/',
+        isFirst: false,
+        isActive: false
+      }
+    };
+
+    const tab = Tab.from([tab1Config, tab2Config]);
+    expect(tab).toMatchObject(expected);
+  });
+
+  it('component supports default tab ordering from config', () => {
+    const tabConfig = [
+      {
+        label: 'This this is my second label',
+        url: '/tab1/',
+        configId: 'tab1'
+      },
+      {
+        label: 'This is my label',
+        url: '/tab2/',
+        configId: 'tab2',
+        isFirst: true
+      }
+    ];
+
+    // Finally construct our component
+    let navComponent = new NavigationComponent({
+      container: '.test-component',
+      tabs: tabConfig
+    });
+
+    const defaultOrder = navComponent.getDefaultTabOrder(tabConfig);
+    expect(defaultOrder).toMatchObject(['tab2', 'tab1']);
+  });
+
+  it('component supports default tab ordering from params', () => {
+    const tabConfig = [
+      {
+        label: 'This this is my second label',
+        url: '/tab1/',
+        configId: 'tab1'
+      },
+      {
+        label: 'This is my label',
+        url: '/tab2/',
+        configId: 'tab2'
+      }
+    ];
+
+    // Finally construct our component
+    let navComponent = new NavigationComponent({
+      container: '.test-component',
+      tabs: tabConfig
+    });
+
+    let params = new URLSearchParams('tabOrder=tab2,tab1');
+    const defaultOrder = navComponent.getDefaultTabOrder(tabConfig, params);
+    expect(defaultOrder).toMatchObject(['tab2', 'tab1']);
+  });
+
+  it('merging two tab orders', () => {
+    const tabConfig = [
+      {
+        label: 'This this is my second label',
+        url: '/tab1/',
+        configId: 'tab1'
+      },
+      {
+        label: 'This is my label',
+        url: '/tab2/',
+        configId: 'tab2'
+      }
+    ];
+
+    // Finally construct our component
+    let navComponent = new NavigationComponent({
+      container: '.test-component',
+      tabs: tabConfig
+    });
+
+    const tabOrder1 = ['tab1', 'tab2'];
+    const tabOrder2 = ['tab2', 'tab3'];
+
+    const defaultOrder = navComponent.mergeTabOrder(tabOrder1, tabOrder2);
+    expect(defaultOrder).toMatchObject(['tab1', 'tab2', 'tab3']);
+  });
+
+  it('generates tab url', () => {
+    const tabConfig = [
+      {
+        label: 'This this is my second label',
+        url: '/tab1/',
+        configId: 'tab1'
+      }
+    ];
+
+    // Finally construct our component
+    let navComponent = new NavigationComponent({
+      container: '.test-component',
+      tabs: tabConfig
+    });
+
+    const params = new URLSearchParams('query=yes');
+    const url = navComponent.generateTabUrl(tabConfig[0].url, params);
+    expect(url).toEqual('/tab1/?query=yes&tabOrder=tab1');
+  });
+});
