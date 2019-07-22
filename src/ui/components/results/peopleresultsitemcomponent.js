54dc9714de91209d7e4e48f6ed694e64dbac3db1
bmcginnis <bmcginnis@yext.com>
2019-07-22 16:12:14 -0400

    templates: allow implementors to set config.templateName to change the template
    config.templateName was defined but not used by any of the Components.  This
    changes each component to respect config.templateName if it's defined and if
    not, fallback to the default templateName for the Component.  For Components
    with multiple allowable templates, this will allow implementors to choose
    a variation without overriding the template parameter.

--- src/ui/components/results/peopleresultsitemcomponent.js
+++ src/ui/components/results/peopleresultsitemcomponent.js
@@ -5,12 +5,19 @@ import ResultsItemComponent from './resultsitemcomponent';
 export default class PeopleResultsItemComponent extends ResultsItemComponent {
   constructor (opts = {}) {
     super(opts);
-
-    this._templateName = 'results/peopleresultsitem';
   }
 
   static get type () {
     return 'PeopleResultsItemComponent';
+  }
+
+  /**
+   * The template to render
+   * @returns {string}
+   * @override
+   */
+  static defaultTemplateName (config) {
+    return 'results/peopleresultsitem';
   }
 
   static areDuplicateNamesAllowed () {
