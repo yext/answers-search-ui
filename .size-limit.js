a30c2fbca6ea633a00b76400e29424c6511d3f45
Billy Bastardi <billy@yext.com>
2019-08-22 12:25:51 -0400

    up the size limit

--- .size-limit.js
+++ .size-limit.js
@@ -1,10 +1,10 @@
 module.exports = [
-    {
-        path: "dist/answers.min.js",
-        limit: "17 KB"
-    },
-    {
-        path: "dist/answerstemplates.compiled.min.js",
-        limit: "50 KB"
-    }
-]
+  {
+    path: 'dist/answers.min.js',
+    limit: '17 KB'
+  },
+  {
+    path: 'dist/answerstemplates.compiled.min.js',
+    limit: '50 KB'
+  }
+];
