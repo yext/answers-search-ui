f1642fc092ecd65303219f3c6eee0ab3162294e9
Ben McGinnis <bmcginnis@yext.com>
2019-09-05 20:11:45 -0400

    templates/icons: add icon component (#179)
    * templates/icons: add icon component
    
    Adds an icon component and a default set of SVG icons from design.  Allows
    implementors to specify an icon by name or provide a custom icon via either URL
    or by providing a string.
    
    Updated the resultssectionheader.hbs and the searchbar.hbs to take advantage of
    this new component.

---
+++ src/ui/icons/briefcase.js
@@ -0,0 +1,5 @@
+import SVGIcon from './icon.js';
+export default new SVGIcon({
+  name: 'briefcase',
+  path: 'M20 7h-4V5c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 20c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V9c0-1.11-.89-2-2-2zm-6 0h-4V5h4v2z'
+});
---
+++ src/ui/icons/kabob.js
@@ -0,0 +1,6 @@
+import SVGIcon from './icon.js';
+export default new SVGIcon({
+  name: 'kabob',
+  viewBox: '0 0 3 11',
+  complexContents: `<circle cx="1.5" cy="1.5" r="1.5"/><circle cx="1.5" cy="5.5" r="1.5"/><circle cx="1.5" cy="9.5" r="1.5"/>`
+});
