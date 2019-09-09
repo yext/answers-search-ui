ffa8787e6455152ea9f0ca1f8824f2df6b5f0376
Julian Delerme <jdelerme@yext.com>
2019-09-09 14:44:52 -0400

    Bump up size limit (#204)
    Effectively remove the size limit since it shouldn't block builds

--- .size-limit.js
+++ .size-limit.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
     path: 'dist/answers.min.js',
-    limit: '25 KB'
+    limit: '100 KB'
   },
   {
     path: 'dist/answerstemplates.compiled.min.js',
-    limit: '50 KB'
+    limit: '100 KB'
   }
 ];
