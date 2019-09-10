6fd6afc7c9807519e388bda63626fcf29fe24cae
Julian Delerme <jdelerme@yext.com>
2019-09-10 13:28:21 -0400

    Use the current version's default template bundle (#210)

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,13 +1,13 @@
 /** @module */
 
 /** The current lib version, reported with errors and analytics */
-export const LIB_VERSION = 'v0.7.5';
+export const LIB_VERSION = 'v0.8.1';
 
 /** The base url for the api backend */
 export const API_BASE_URL = 'https://liveapi.yext.com';
 
 /** The default url for compiled component templates */
-export const COMPILED_TEMPLATES_URL = 'https://assets.sitescdn.net/answers/answerstemplates.compiled.min.js';
+export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
 
 /** The base url for the analytics backend  */
 export const ANALYTICS_BASE_URL = 'https://realtimeanalytics.yext.com';
