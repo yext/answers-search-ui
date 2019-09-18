69983031f896d33204cc9a8f28c1a0b639d3ac10
Julian Delerme <jdelerme@yext.com>
2019-09-18 17:34:40 -0400

    Add IE bundle

--- conf/gulp-tasks/library.gulpfile.js
+++ conf/gulp-tasks/library.gulpfile.js
@@ -4,6 +4,7 @@ const rollup = require('gulp-rollup-lightweight');
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const insert = require('rollup-plugin-insert');
 
 const source = require('vinyl-source-stream');
 const rename = require('gulp-rename');
@@ -31,6 +32,7 @@ function bundle () {
         include: './node_modules/**'
       }),
       babel({
+        babelrc: false,
         exclude: 'node_modules/**',
         presets: ['@babel/env']
       })
@@ -40,9 +42,67 @@ function bundle () {
     .pipe(dest('dist'));
 }
 
-function minifyJS (cb) {
+function legacyBundle () {
+  return rollup({
+    input: './src/answers-umd.js',
+    output: {
+      format: 'iife',
+      name: NAMESPACE,
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      insert.prepend(`
+        import '@babel/polyfill/noConflict';
+        import 'whatwg-fetch';
+        if (!Element.prototype.matches) { // Element.matches polyfill from MDN
+          Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                                      Element.prototype.webkitMatchesSelector;
+        }`,
+      {
+        include: './src/answers-umd.js'
+      }),
+      commonjs({
+        include: './node_modules/**'
+      }),
+      babel({
+        runtimeHelpers: true,
+        babelrc: false,
+        exclude: 'node_modules/**',
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              'loose': true,
+              'modules': false,
+              'corejs': 3,
+              'useBuiltIns': 'entry'
+            }
+          ]
+        ],
+        plugins: [
+          '@babel/syntax-dynamic-import',
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-transform-arrow-functions',
+          '@babel/plugin-proposal-object-rest-spread'
+        ]
+      })
+    ]
+  })
+    .pipe(source('answers-legacy.js'))
+    .pipe(dest('dist'));
+}
+
+function minifyJS () {
   return src('./dist/answers.js')
     .pipe(rename('answers.min.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'));
+}
+
+function minifyLegacy () {
+  return src('./dist/answers-legacy.js')
+    .pipe(rename('answers-legacy.min.js'))
     .pipe(uglify())
     .pipe(dest('dist'));
 }
@@ -69,7 +129,7 @@ function watchCSS (cb) {
 }
 
 exports.default = parallel(
-  series(bundle, minifyJS),
+  series(bundle, legacyBundle, minifyJS, minifyLegacy),
   series(compileCSS)
 );
 
