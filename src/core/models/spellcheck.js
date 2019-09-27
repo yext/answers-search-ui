37ab5320cf81b12fe5a017a73aa858cb769e18a8
Bowen Wang <bwang@yext.com>
2019-09-27 14:09:08 -0400

    Generate correctedQueryUrl in SpellCheck core model and update README

--- src/core/models/spellcheck.js
+++ src/core/models/spellcheck.js
@@ -1,6 +1,7 @@
 /** @module SpellCheck */
 
 import HighlightedValue from './highlightedvalue';
+import SearchParams from '../../ui/dom/searchparams';
 
 /**
  * SpellCheck is the core state model
@@ -19,6 +20,7 @@ export default class SpellCheck {
      * @type {string}
      */
     this.correctedQuery = data.correctedQuery || null;
+
     /**
      * The corrected query with highlighted tags
      * @type {string}
@@ -30,6 +32,24 @@ export default class SpellCheck {
      * @type {string}
      */
     this.type = data.type || null;
+
+    /**
+     * Should show spell check or not
+     * @type {boolean}
+     */
+    this.shouldShow = this.correctedQuery !== null;
+
+    /**
+     * The redirect url for the corrected query
+     * @type {string}
+     */
+    this.correctedQueryUrl = this._buildRedirectQueryUrl(this.correctedQuery, this.type);
+
+    /**
+     * The help text such as Did you mean
+     * @type {string}
+     */
+    this.helpText = this._getHelpText(this.type);
   }
 
   /**
@@ -47,5 +67,25 @@ export default class SpellCheck {
       correctedQueryDisplay: new HighlightedValue(response.correctedQuery).get(),
       type: response.type
     });
+  }
+
+  _buildRedirectQueryUrl (query, type) {
+    if (query === null) {
+      return '';
+    }
+    let params = new SearchParams(window.location.search.substring(1));
+    params.set('query', query.value);
+    params.set('skipSpellCheck', true);
+    params.set('queryTrigger', type.toLowerCase());
+    return window.location.href.split('?')[0] + '?' + params.toString();
+  }
+
+  _getHelpText (type) {
+    switch (type) {
+      case 'SUGGEST':
+        return 'Did you mean:';
+      default:
+        return '';
+    }
   }
 }
--- src/ui/components/search/spellcheckcomponent.js
+++ src/ui/components/search/spellcheckcomponent.js
@@ -1,12 +1,11 @@
 /** @module SpellCheckComponent */
 
 import Component from '../component';
-import SearchParams from '../../dom/searchparams';
 import StorageKeys from '../../../core/storage/storagekeys';
 
 /**
  * SpellCheckComponent will support displaying suggestion, autocorrect and combined(maybe in the future)
- * provided from speelling correction.
+ * provided from spelling correction.
  *
  * @extends Component
  */
@@ -25,35 +24,8 @@ export default class SpellCheckComponent extends Component {
     return 'search/spellcheck';
   }
 
-  init (opts) {
-    super.init(opts);
-    return this;
-  }
-
-  setState (data, val) {
-    return super.setState(Object.assign(data, {
-      shouldShow: data.correctedQuery !== undefined,
-      correctedQueryUrl: this._buildRedirectQueryUrl(data.correctedQuery),
-      helpText: this._getHelpText(data.type)
-    }, val));
-  }
-
-  _buildRedirectQueryUrl (query) {
-    if (query === undefined) {
-      return '';
-    }
-    let params = new SearchParams(window.location.search.substring(1));
-    params.set('query', query);
-    params.set('spellCheck', false);
-    return window.location.href.split('?')[0] + '?' + params.toString();
-  }
-
-  _getHelpText (type) {
-    switch (type) {
-      case 'SUGGEST':
-        return 'Did you mean:';
-      default:
-        return '';
-    }
+  onCreate () {
+    this.core.persistentStorage.delete('skipSpellCheck');
+    this.core.persistentStorage.delete('queryTrigger');
   }
 }
