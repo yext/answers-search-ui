1937181fb763dfe07252fe4183eb313bef8851ea
Ben McGinnis <bmcginnis@yext.com>
2019-10-04 11:49:54 -0400

    add close icon (#266)

---
+++ src/ui/icons/close.js
@@ -0,0 +1,9 @@
+import SVGIcon from './icon.js';
+export default new SVGIcon({
+  name: 'close',
+  complexContents: `
+    <path d="M7 8l9.716 9.716m0-9.716L7 17.716" 
+          stroke="currentColor" 
+          stroke-width="2"/>
+  `
+});
