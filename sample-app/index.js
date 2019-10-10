1b830b15cd11563fedfc4be897709ff0af262023
Adrian Mullings <amullings@yext.com>
2019-10-09 20:54:50 -0400

    Add sample Answers app (#275)
    * Add sample Answers app
    
    Screenshots:
    https://drive.google.com/a/yext.com/file/d/1tUJiAdJub2JIecwP16d6dYHZC8Ws4PK1/view?usp=drivesdk
    https://drive.google.com/a/yext.com/file/d/1TKXNYT2JKRnrNmA9llHrz8EGRFQX3uy-/view?usp=drivesdk
    https://drive.google.com/a/yext.com/file/d/1OFerbqJR6y0xaVfS-DgNHPUP5X57lqNX/view?usp=drivesdk

---
+++ sample-app/index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const writeFile = util.promisify(fs.writeFile);
+
+const Handlebars = require('handlebars');
+const metadataTemplate = Handlebars.compile(
+  fs.readFileSync('./metadata.hbs', 'utf8')
+);
+const universalTemplate = Handlebars.compile(
+  fs.readFileSync('./universal.hbs', 'utf8')
+);
+const verticalTemplate = Handlebars.compile(
+  fs.readFileSync('./vertical.hbs', 'utf8')
+);
+Handlebars.registerPartial('metadata', metadataTemplate);
+
+const credentials = require('./credentials.json');
+const configFile = process.argv[2]
+  ? path.join(process.cwd(), process.argv[2])
+  : './sample-config.json';
+const siteConfig = require(configFile);
+const config = Object.assign({}, credentials, siteConfig);
+
+const bundleUrl = `../dist/answers.js`;
+const cssUrl = `../dist/answers.css`;
+
+function main () {
+  writeUniversalHtml();
+
+  for (const verticalKey of Object.keys(config.verticals)) {
+    writeVerticalHtml(verticalKey);
+  }
+}
+
+function writeUniversalHtml () {
+  const html = universalTemplate({
+    bundleUrl,
+    cssUrl,
+    title: 'Universal Search Experience Test',
+    configJson: new Handlebars.SafeString(JSON.stringify(config))
+  });
+  writeFile('index.html', html)
+    .catch(console.error);
+}
+
+function writeVerticalHtml (verticalKey) {
+  const configWithVertical = Object.assign({}, config, {
+    verticalKey
+  });
+  const html = verticalTemplate({
+    bundleUrl,
+    cssUrl,
+    title: `${capitalize(verticalKey)} Experience Test`,
+    configJson: new Handlebars.SafeString(JSON.stringify(configWithVertical))
+  });
+  writeFile(`${verticalKey}.html`, html)
+    .catch(console.error);
+}
+
+function capitalize (s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+main();
