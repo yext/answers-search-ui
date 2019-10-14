0054893ac81bbe57253d77f542f1e76c67be75ac
Vignesh Ramesh <vramesh@yext.com>
2019-10-14 12:01:41 -0400

    undoing change to LIVE API URL

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,7 +4,7 @@
 export const LIB_VERSION = 'v0.9.0';
 
 /** The base url for the api backend */
-export const API_BASE_URL = 'https://liveapi-dev.yext.com';
+export const API_BASE_URL = 'https://liveapi.yext.com';
 
 /** The default url for compiled component templates */
 export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
