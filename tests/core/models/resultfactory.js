6b30f47f6dbef2b38aab24d06fdc4283afde6c2e
Vignesh Ramesh <vramesh@yext.com>
2019-10-14 11:55:52 -0400

    JS SDK : Apply Highlighting to Knowledge Manager Entity Field Values
    J=WAT-467
    TEST=auto,manual
    
    List of Changes :
    1) Result Factory separation for Knowledge Manager
    - previously, Knowledge Manager and Custom searchers fell into a Generic Result Factory function
    - creating a separate Result Factory function to create Results for Knowledge Manager backends
    - refactor to move KM Result creation and Field Formatting logic into this function
    2) Highlighting
    - KM backends return highlighting instructions when available to highlight entity field values
    - apply this highlighting to substrings within entity field values
    - adding a property to the Result class to contain highlighted fields
    - in addition to the property that contains the raw (without highlighting) entity profile data
    3) Highlighting available to Field Formatters
    - pass in the highlighted field value to field formatters functions
    - developers can choose to use either the raw entity field value or the highlighted version
    4) Field Formatter interface
    - modified the interface for Field Formatters to take in an object containing named formatter params
    - instead of a growingly long list of parameters
    5) Scope of Highlighter member function
    - to consolidate logic to manipulate strings for highlighting
    - re-using the HighlightedValue class' buildHighlightedValue function
    - this was previously a private member function
    - making it public for use by ResultFactory as well
    6) Automated Tests
    - added test suite for highlighting entity profile data
    - changed field formatting test suite to use the modified field formatter interface

--- tests/core/models/resultfactory.js
+++ tests/core/models/resultfactory.js
@@ -24,7 +24,7 @@ describe('formatting data', () => {
       }
     }];
 
-    const results = ResultFactory.from(data, {}, null);
+    const results = ResultFactory.from(data, {}, null, 'KNOWLEDGE_MANAGER');
     expect(results[0].title).toBe('Test');
     expect(results[0].details).toBe('Result description');
     expect(results[0]._formatted).toEqual({});
@@ -38,17 +38,125 @@ describe('formatting data', () => {
       }
     }];
 
-    const formatters = {
-      'name': name => name.toUpperCase(),
-      'description': desc => desc.substr(7)
+    let nameFormatter = formatterParams => {
+      return formatterParams.entityFieldValue.toUpperCase();
     };
 
-    const results = ResultFactory.from(data, formatters, null);
+    let descriptionFormatter = formatterParams => {
+      return formatterParams.entityFieldValue.substr(7);
+    };
+
+    const formatters = {
+      'name': nameFormatter,
+      'description': descriptionFormatter
+    };
+
+    const results = ResultFactory.from(data, formatters, null, 'KNOWLEDGE_MANAGER');
     expect(results[0].title).toBe('TEST');
     expect(results[0].details).toBe('description');
     expect(results[0]._formatted).toEqual({
       name: 'TEST',
       description: 'description'
+    });
+  });
+});
+
+describe('highlighting data', () => {
+  const data = {
+    name: 'ATM',
+    address: {
+      line1: '1745 Broadway',
+      city: 'New York',
+      region: 'NY'
+    },
+    featuredMessage: {
+      description: 'Save time & bank on your terms at over 1,800 ATMs'
+    },
+    parentLevel1: {
+      parentLevel2: {
+        child1: 'Save time & bank on your terms at over 1,800 ATMs',
+        child2: 'Save time & bank on your terms at over 1,800 ATMs'
+
+      }
+    }
+  };
+
+  it('highlighted data should be empty when no highlighted fields specified', () => {
+    const highlightedData = ResultFactory.getHighlightedData(data, {});
+    expect(highlightedData).toEqual({});
+  });
+
+  it('highlighted data should highlight & contain only fields with highlighting specified', () => {
+    const highlightedFields = {
+      name: {
+        matchedSubstrings: [{
+          length: 3,
+          offset: 0
+        }],
+        value: 'ATM'
+      }
+    };
+
+    const outputHighlightedData = ResultFactory.getHighlightedData(data, highlightedFields);
+
+    expect(outputHighlightedData).toEqual({
+      name: '<strong>ATM</strong>'
+    });
+  });
+
+  it('highlighter should highlight nested highlighted fields', () => {
+    const highlightedFields = {
+      featuredMessage: {
+        description: {
+          matchedSubstrings: [{
+            length: 4,
+            offset: 45
+          }],
+          value: 'Save time & bank on your terms at over 1,800 ATMs'
+        }
+      }
+    };
+
+    const outputHighlightedData = ResultFactory.getHighlightedData(data, highlightedFields);
+
+    expect(outputHighlightedData).toEqual({
+      featuredMessage: {
+        description: 'Save time & bank on your terms at over 1,800 <strong>ATMs</strong>'
+      }
+    });
+  });
+
+  it('highlighter should highlight deeply nested highlighted fields', () => {
+    const highlightedFields = {
+      parentLevel1: {
+        parentLevel2: {
+          child1: {
+            matchedSubstrings: [{
+              length: 4,
+              offset: 45
+            }],
+            value: 'Save time & bank on your terms at over 1,800 ATMs'
+          },
+          child2: {
+            matchedSubstrings: [{
+              length: 4,
+              offset: 45
+            }],
+            value: 'Save time & bank on your terms at over 1,800 ATMs'
+          }
+        }
+      }
+    };
+
+    const outputHighlightedData = ResultFactory.getHighlightedData(data, highlightedFields);
+
+    expect(outputHighlightedData).toEqual({
+      parentLevel1: {
+        parentLevel2: {
+          child1: 'Save time & bank on your terms at over 1,800 <strong>ATMs</strong>',
+          child2: 'Save time & bank on your terms at over 1,800 <strong>ATMs</strong>'
+        }
+      }
     });
   });
 });
