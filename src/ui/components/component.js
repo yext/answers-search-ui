0df59a296061935c55bb70f16a816bca371ca1ed
Billy Bastardi <wabism@gmail.com>
2019-10-17 00:02:53 -0400

    ui/component: datatransform should propogate the result to subcomponents

--- src/ui/components/component.js
+++ src/ui/components/component.js
@@ -340,7 +340,10 @@ export default class Component {
     // Process the DOM to determine if we should create
     // in-memory sub-components for rendering
     let domComponents = DOM.queryAll(this._container, '[data-component]:not([data-is-component-mounted])');
-    domComponents.forEach(c => this._createSubcomponent(c, this._state.get()));
+    domComponents.forEach(c => {
+      const data = this.transformData(JSON.parse(JSON.stringify(this._state.get())));
+      this._createSubcomponent(c, data);
+    });
 
     this._children.forEach(child => {
       child.mount();
