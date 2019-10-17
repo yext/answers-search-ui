0e06902a7a4387336d4d4960d3e022094e06a9a4
Billy Bastardi <wabism@gmail.com>
2019-10-17 00:32:37 -0400

    ui/component: style nit--use const

--- src/ui/components/component.js
+++ src/ui/components/component.js
@@ -339,8 +339,7 @@ export default class Component {
 
     // Process the DOM to determine if we should create
     // in-memory sub-components for rendering
-    let domComponents = DOM.queryAll(this._container, '[data-component]:not([data-is-component-mounted])');
-
+    const domComponents = DOM.queryAll(this._container, '[data-component]:not([data-is-component-mounted])');
     const data = this.transformData(JSON.parse(JSON.stringify(this._state.get())));
     domComponents.forEach(c => this._createSubcomponent(c, data));
 
