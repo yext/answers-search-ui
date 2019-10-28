033aadbdcd0d4a3dbe52feb503f9482ea8694557
Kelly Yu <kyu@yext.com>
2019-10-28 13:06:41 -0400

    core/search: add CodeLab kelly searcher

--- src/core/search/searchapi.js
+++ src/core/search/searchapi.js
@@ -128,4 +128,34 @@ export default class SearchApi {
     return request.get()
       .then(response => response.json());
   }
+
+  /**
+   * Initiate a kelly search
+   * @param {boolean} includeLastName include last name in the search
+   * @param {boolean} useUserName user username instead of real name
+   * @param {string} additionalSearchText more text to add to theeasrch
+   */
+  kellySearch (includeLastName, useUserName, additionalSearchText) {
+    let name = includeLastName ? 'Kelly Yu' : 'Kelly';
+    name = useUserName ? 'kyu' : name;
+    if (additionalSearchText && typeof additionalSearchText !== 'string') {
+      throw new AnswersCoreError('additionalSearchText must be a string', 'kellySearch');
+    }
+    const query = `${name} ${additionalSearchText}`;
+
+    let request = new ApiRequest({
+      endpoint: '/v2/accounts/me/answers/query',
+      apiKey: this._apiKey,
+      version: this._version,
+      params: {
+        'input': query,
+        'experienceKey': this._experienceKey,
+        'version': this._experienceVersion,
+        'locale': this._locale
+      }
+    });
+
+    return request.get()
+      .then(response => response.json());
+  }
 }
