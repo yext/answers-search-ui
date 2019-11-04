e03c5310fc584597c4197f28202527ad7baf2c3f
Vignesh Ramesh <vramesh@yext.com>
2019-11-04 18:37:53 -0500

    undo change to constants.js

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,11 +4,10 @@
 export const LIB_VERSION = 'v0.9.2';
 
 /** The base url for the api backend */
-export const API_BASE_URL = 'https://liveapi-dev.yext.com';
+export const API_BASE_URL = 'https://liveapi.yext.com';
 
 /** The default url for compiled component templates */
-//export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
-export const COMPILED_TEMPLATES_URL = `/home/vramesh/Projects/answers_js/answers/dist/answerstemplates.compiled.min.js`;
+export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
 
 /** The base urls for the analytics backend  */
 export const ANALYTICS_BASE_URL = 'https://realtimeanalytics.yext.com';
