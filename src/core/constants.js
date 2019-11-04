aa1a31c8beca17069bb69ca74c53e5b24b011e55
Vignesh Ramesh <vramesh@yext.com>
2019-11-04 17:30:12 -0500

    Q&A Component styling changes

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,10 +4,11 @@
 export const LIB_VERSION = 'v0.9.3';
 
 /** The base url for the api backend */
-export const API_BASE_URL = 'https://liveapi.yext.com';
+export const API_BASE_URL = 'https://liveapi-dev.yext.com';
 
 /** The default url for compiled component templates */
-export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
+//export const COMPILED_TEMPLATES_URL = `https://assets.sitescdn.net/answers/${LIB_VERSION}/answerstemplates.compiled.min.js`;
+export const COMPILED_TEMPLATES_URL = `/home/vramesh/Projects/answers_js/answers/dist/answerstemplates.compiled.min.js`;
 
 /** The base urls for the analytics backend  */
 export const ANALYTICS_BASE_URL = 'https://realtimeanalytics.yext.com';
