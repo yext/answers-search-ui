85654c6c6b7badec6e6bc144b4ce448714ffa909
Vignesh Ramesh <vramesh@yext.com>
2019-11-04 17:36:14 -0500

    undo change to constants

--- sample-app/public/app.js
+++ sample-app/public/app.js
@@ -5,7 +5,7 @@ const config = JSON.parse(document.getElementById('config').textContent);
 window.initAnswers = async function initAnswers () {
   const {
     apiKey,
-    experienceKey,
+    answersKey,
     verticalKey,
     mapProvider,
     mapApiKey
@@ -21,7 +21,7 @@ window.initAnswers = async function initAnswers () {
 
   ANSWERS.init({
     apiKey,
-    experienceKey,
+    answersKey,
     onReady: function () {
       ANSWERS.addComponent('Navigation', {
         container: '.navigation-container',
