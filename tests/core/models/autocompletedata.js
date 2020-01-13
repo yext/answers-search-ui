671f1fee74defaf00f3e355b38030a8286a27ca7
Tom Meyer <tmeyer@yext.com>
2020-01-13 15:38:11 -0500

    SearchBar: Handle duplicate searches with near me intent. (#444)
    * SearchBar: Handle duplicate searches with near me intent.
    
    With this CR, before a query is issued, we compute the intent of the query and,
    if appropriate, attempt to get location information. Namely, if the intent of
    the query is NEAR_ME, we will attempt to get location information before
    issuing the actual query. This information, if the user consents, will be
    added to the query when it is issued. This is in contrast to the current
    behavior of the system. Currently, the SearchBar issues the query without
    first checking it's intent. Once that query is performed, we attempt to get
    location info from the user (if the query had a NEAR_ME intent). If we get this
    information, we perform a second query with the location info. This CR
    simplifies the process by not issuing the query until we've had a chance to get
    the location information. Note that this behavior of checking intents before
    issuing the query can be turned off by disabling promptForLocation in the
    SearchBar's config.
    
    Part of the requirements for this item were to remove all references to the
    PermissionsAPI from the SearchBar component.
    
    J=ANS-404
    TEST=auto, manual
    
    Fixed the Jest tests broken by this change. Added a new unit test for the
    AutocompleteData class. Tested the following scenarios manually:
    
    - Location info was initially unset in Chrome. Picked an autocomplete option
      that had NEAR_ME intent. I allowed the site to access my location. In the
      Network tab, I saw exactly one query was issued and it included my location
      info. I manually entered a second query with NEAR_ME intent. I was not
      prompted again to allow access to my location info. Again, exactly one query
      was issued and it had my location info.
    - Location info was initially unset in Chrome. Entered a query with NEAR_ME
      intent. Denied access to my location. Ensured only one query was issued and
      it did not include location information. Entered another query with NEAR_ME
      intent to ensure I wasn't again prompted for access to my location.
    - Location info was initially unset in Chrome. I performed a search by editing
      the query param in the site's URL. The query had NEAR_ME intent. I was
      prompted for access to my location. I granted the access and verified only
      one query was issued and that it had my location info. Navigated to a
      vertical tab and ensured I wasn't prompted for that info again. Made sure
      one query was executed on that page and it had my location info.
    - Disabled promptForLocation. Performed a query with NEAR_ME intent. Ensured
      I was not prompted for my location info and only one query was executed.

--- tests/core/models/autocompletedata.js
+++ tests/core/models/autocompletedata.js
@@ -3,9 +3,12 @@ import AutoCompleteData from '../../../src/core/models/autocompletedata';
 describe('constructing from a response', () => {
   it('creates a section if response has no sections', () => {
     const response = {
+      input: {
+        queryIntents: []
+      },
       results: [
-        { key: 'jesse', value: 'Jesse Sharps' },
-        { key: 'billy', value: 'Billy Bastardi' }
+        { key: 'jesse', value: 'Jesse Sharps', queryIntents: [] },
+        { key: 'billy', value: 'Billy Bastardi', queryIntents: [] }
       ]
     };
 
@@ -16,7 +19,8 @@ describe('constructing from a response', () => {
         shortValue: 'Jesse Sharps',
         filter: {},
         matchedSubstrings: [],
-        highlightedValue: 'Jesse Sharps'
+        highlightedValue: 'Jesse Sharps',
+        intents: []
       },
       {
         key: 'billy',
@@ -24,7 +28,8 @@ describe('constructing from a response', () => {
         shortValue: 'Billy Bastardi',
         filter: {},
         matchedSubstrings: [],
-        highlightedValue: 'Billy Bastardi'
+        highlightedValue: 'Billy Bastardi',
+        intents: []
       }
     ];
 
@@ -39,16 +44,21 @@ describe('constructing from a response', () => {
 
   it('properly creates highlightedValue from matchedSubstrings', () => {
     const response = {
+      input: {
+        queryIntents: []
+      },
       results: [
         {
           key: 'jesse',
           value: 'Jesse Sharps',
-          matchedSubstrings: [ { offset: 8, length: 2 } ]
+          matchedSubstrings: [ { offset: 8, length: 2 } ],
+          queryIntents: []
         },
         {
           key: 'billy',
           value: 'Billy Bastardi',
-          matchedSubstrings: [ { offset: 10, length: 2 } ]
+          matchedSubstrings: [ { offset: 10, length: 2 } ],
+          queryIntents: []
         }
       ]
     };
@@ -60,7 +70,8 @@ describe('constructing from a response', () => {
         shortValue: 'Jesse Sharps',
         filter: {},
         matchedSubstrings: [ { offset: 8, length: 2 } ],
-        highlightedValue: '<strong>Jesse Sh</strong>ar<strong>ps</strong>'
+        highlightedValue: '<strong>Jesse Sh</strong>ar<strong>ps</strong>',
+        intents: []
       },
       {
         key: 'billy',
@@ -68,7 +79,8 @@ describe('constructing from a response', () => {
         shortValue: 'Billy Bastardi',
         filter: {},
         matchedSubstrings: [ { offset: 10, length: 2 } ],
-        highlightedValue: '<strong>Billy Bast</strong>ar<strong>di</strong>'
+        highlightedValue: '<strong>Billy Bast</strong>ar<strong>di</strong>',
+        intents: []
       }
     ];
 
@@ -80,6 +92,9 @@ describe('constructing from a response', () => {
 
   it('properly highlights multiple matchedSubstrings', () => {
     const response = {
+      input: {
+        queryIntents: []
+      },
       sections: [
         {
           label: 'Team Watson',
@@ -87,7 +102,8 @@ describe('constructing from a response', () => {
             {
               key: 'jesse',
               value: 'Jesse Sharps',
-              matchedSubstrings: [ { offset: 1, length: 3 }, { offset: 7, length: 4 } ]
+              matchedSubstrings: [ { offset: 1, length: 3 }, { offset: 7, length: 4 } ],
+              queryIntents: []
             }
           ]
         }
@@ -101,7 +117,8 @@ describe('constructing from a response', () => {
         shortValue: 'Jesse Sharps',
         filter: {},
         matchedSubstrings: [ { offset: 1, length: 3 }, { offset: 7, length: 4 } ],
-        highlightedValue: '<strong>J</strong>ess<strong>e S</strong>harp<strong>s</strong>'
+        highlightedValue: '<strong>J</strong>ess<strong>e S</strong>harp<strong>s</strong>',
+        intents: []
       }
     ];
 
@@ -113,6 +130,9 @@ describe('constructing from a response', () => {
 
   it('properly handles full length match', () => {
     const response = {
+      input: {
+        queryIntents: []
+      },
       sections: [
         {
           label: 'Team Watson',
@@ -120,7 +140,8 @@ describe('constructing from a response', () => {
             {
               key: 'jesse',
               value: 'Jesse',
-              matchedSubstrings: [ { offset: 0, length: 5 } ]
+              matchedSubstrings: [ { offset: 0, length: 5 } ],
+              queryIntents: []
             }
           ]
         }
@@ -134,11 +155,53 @@ describe('constructing from a response', () => {
         shortValue: 'Jesse',
         filter: {},
         matchedSubstrings: [ { offset: 0, length: 5 } ],
-        highlightedValue: 'Jesse'
+        highlightedValue: 'Jesse',
+        intents: []
       }
     ];
 
     const data = AutoCompleteData.from(response);
+    const actualResults = data.sections[0].results;
+    expect(actualResults).toHaveLength(1);
+    expect(actualResults).toEqual(expectedResults);
+  });
+
+  it('properly handles queryIntents', () => {
+    const inputIntents = ['INTENT_1'];
+    const resultIntents = ['INTENT_2'];
+    const response = {
+      input: {
+        queryIntents: inputIntents
+      },
+      sections: [
+        {
+          label: 'Team Watson',
+          results: [
+            {
+              key: 'jesse',
+              value: 'Jesse',
+              matchedSubstrings: [ { offset: 0, length: 5 } ],
+              queryIntents: resultIntents
+            }
+          ]
+        }
+      ]
+    };
+
+    const expectedResults = [
+      {
+        key: 'jesse',
+        value: 'Jesse',
+        shortValue: 'Jesse',
+        filter: {},
+        matchedSubstrings: [ { offset: 0, length: 5 } ],
+        highlightedValue: 'Jesse',
+        intents: resultIntents
+      }
+    ];
+
+    const data = AutoCompleteData.from(response);
+    expect(data.inputIntents).toEqual(inputIntents);
     const actualResults = data.sections[0].results;
     expect(actualResults).toHaveLength(1);
     expect(actualResults).toEqual(expectedResults);
