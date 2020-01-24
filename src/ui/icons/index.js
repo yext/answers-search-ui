60ed373a4e74eff89b5b815b8932ae0b2f8e198c
Oliver Shi <oshi@yext.com>
2020-01-24 16:42:06 -0500

    ANS-319: Default Styling - Update Default Icons
    This commit adds additional icons, as well as updates the briefcase icons.
    It also fixes the star icon's name from receipt to star, and changes all remaining
    imports in icons/index to use extensionless imports.
    
    TEST=manual
    Loaded up a test site that added every icon in icons/index.js to the page.

--- src/ui/icons/index.js
+++ src/ui/icons/index.js
@@ -1,30 +1,50 @@
-import thumbIcon from './thumb.js';
-import receiptIcon from './receipt.js';
-import pantheonIcon from './pantheon.js';
-import micIcon from './mic.js';
-import directionsIcon from './directions.js';
-import calendarIcon from './calendar.js';
-import calloutIcon from './callout.js';
-import infoIcon from './info.js';
-import briefcaseIcon from './briefcase.js';
-import kabobIcon from './kabob.js';
-import personIcon from './person.js';
-import magnifyingGlassIcon from './magnifying_glass.js';
-import officeIcon from './office.js';
-import linkIcon from './link.js';
-import windowIcon from './window.js';
-import phoneIcon from './phone.js';
-import tagIcon from './tag.js';
-import documentIcon from './document.js';
-import chevronIcon from './chevron.js';
-import supportIcon from './support.js';
-import yextIcon from './yext.js';
-import pinIcon from './pin.js';
-import gearIcon from './gear.js';
-import lightBulbIcon from './light_bulb.js';
-import starIcon from './star.js';
-import close from './close.js';
+import thumbIcon from './thumb';
+import receiptIcon from './receipt';
+import pantheonIcon from './pantheon';
+import micIcon from './mic';
+import directionsIcon from './directions';
+import calendarIcon from './calendar';
+import calloutIcon from './callout';
+import infoIcon from './info';
+import briefcaseIcon from './briefcase';
+import kabobIcon from './kabob';
+import personIcon from './person';
+import magnifyingGlassIcon from './magnifying_glass';
+import officeIcon from './office';
+import linkIcon from './link';
+import windowIcon from './window';
+import phoneIcon from './phone';
+import tagIcon from './tag';
+import documentIcon from './document';
+import chevronIcon from './chevron';
+import supportIcon from './support';
+import yextIcon from './yext';
+import pinIcon from './pin';
+import gearIcon from './gear';
+import lightBulbIcon from './light_bulb';
+import starIcon from './star';
+import close from './close';
 import elements from './elements';
+import application from './application';
+import appointment from './appointment';
+import book from './book';
+import calendarWithClock from './calendar_with_clock';
+import call from './call';
+import cart from './cart';
+import demo from './demo';
+import details from './details';
+import dollarSign from './dollar_sign';
+import email from './email';
+import fourGrid from './four_grid';
+import linkInCircle from './link_in_circle';
+import network from './network';
+import officePin from './office_pin';
+import personWithCircle from './person_with_circle';
+import pillars from './pillars';
+import RSVP from './RSVP';
+import sixGrid from './six_grid';
+import supportSquare from './support_square';
+import webpage from './webpage';
 
 const iconsArray = [
   thumbIcon,
@@ -52,7 +72,27 @@ const iconsArray = [
   gearIcon,
   lightBulbIcon,
   elements,
-  close
+  close,
+  application,
+  appointment,
+  book,
+  calendarWithClock,
+  call,
+  cart,
+  demo,
+  details,
+  dollarSign,
+  email,
+  fourGrid,
+  linkInCircle,
+  network,
+  officePin,
+  personWithCircle,
+  pillars,
+  RSVP,
+  sixGrid,
+  supportSquare,
+  webpage
 ];
 
 const Icons = {};
---
+++ src/ui/icons/pillars.js
@@ -0,0 +1,18 @@
+import SVGIcon from './icon.js';
+export default new SVGIcon({
+  name: 'pillars',
+  viewBox: '0 0 22 12',
+  complexContents: `
+    <g id="Layouts" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="Examples/Yext.com" transform="translate(-52.000000, -3698.000000)" fill="#454B4F">
+          <g id="Elements/Icons/Vertical/Packages" transform="translate(52.000000, 3696.000000)">
+              <g id="Icon---Packages" transform="translate(0.916667, 2.666667)">
+                  <rect id="Rectangle" x="0" y="1.33333333" width="4.58333333" height="8" rx="1"></rect>
+                  <rect id="Rectangle-Copy-2" x="6.41666667" y="0" width="7.33333333" height="10.6666667" rx="1"></rect>
+                  <rect id="Rectangle-Copy" x="15.5833333" y="1.33333333" width="4.58333333" height="8" rx="1"></rect>
+              </g>
+          </g>
+      </g>
+    </g>
+  `
+});
