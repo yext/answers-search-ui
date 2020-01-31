a841f622afe99b8ac2d455f506a1dc1e048cb77d
Oliver Shi <oshi@yext.com>
2020-01-31 13:52:02 -0500

    move IconComponent dependency purely into mockManager

--- tests/setup/managermocker.js
+++ tests/setup/managermocker.js
@@ -6,14 +6,15 @@ import path from 'path';
 import HandlebarsRenderer from '../../src/ui/rendering/handlebarsrenderer';
 import Handlebars from 'handlebars/dist/handlebars.min.js';
 import MockComponentManager from './mockcomponentmanager';
-
+import IconComponent from '../../src/ui/components/icons/iconcomponent';
 /**
  * Generates a MockComponentManager with templates from the passed in template paths.
  * TODO(oshi): better module/method names
  */
 export default function mockManager (mockedCore, ...templatePaths) {
   const rendererOpts = {
-    '_hb': Handlebars
+    '_hb': Handlebars,
+    [IconComponent.defaultTemplateName()]: Handlebars.compile(loadTemplate(IconComponent.defaultTemplateName()))
   };
 
   for (let i = 0; i < templatePaths.length; i++) {
