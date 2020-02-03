5cadfb09b25ec1e73f802c37dc19dde13433e65a
Oliver Shi <oshi@yext.com>
2020-02-03 11:18:30 -0500

    Remove deprecated item templates (#480)
    * Remove deprecated item templates
    
    * fix tests
    
    * add back accordion + custom render/templates
    
    * clean up renderItem + itemTemplate for vertical results
    only allow function and string respectively, since
    the capability to pass a map to both was not working properly
    anyways and is not the direction we wish to go in the future
    
    * fix semistandard
    
    * update readme

---
+++ tests/ui/components/results/verticalresultscomponent.js
@@ -0,0 +1,40 @@
+import DOM from '../../../../src/ui/dom/dom';
+import { mount } from 'enzyme';
+import mockManager from '../../../setup/managermocker';
+import ResultsComponent from '../../../../src/ui/components/results/resultscomponent';
+import IconComponent from '../../../../src/ui/components/icons/iconcomponent';
+
+const mockedCore = () => ({
+  globalStorage: {
+    getState: () => {}
+  }
+});
+
+DOM.setup(document, new DOMParser());
+
+const COMPONENT_MANAGER = mockManager(
+  mockedCore(),
+  ResultsComponent.defaultTemplateName(),
+  IconComponent.defaultTemplateName()
+);
+
+describe('vertical results component', () => {
+  let defaultConfig;
+
+  beforeEach(() => {
+    const bodyEl = DOM.query('body');
+    DOM.empty(bodyEl);
+    DOM.append(bodyEl, DOM.createEl('div', { id: 'test-component' }));
+
+    defaultConfig = {
+      container: '#test-component',
+      verticalKey: 'verticalKey'
+    };
+  });
+
+  it('renders with only default config', () => {
+    const component = COMPONENT_MANAGER.create(ResultsComponent.type, defaultConfig);
+    const wrapper = mount(component);
+    expect(wrapper).toBeTruthy();
+  });
+});
