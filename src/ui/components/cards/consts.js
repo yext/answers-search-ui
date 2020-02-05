6ce7adaeef6da20b8e2acf0b6ad97c3d7feb658a
Oliver Shi <oshi@yext.com>
2020-02-05 11:27:15 -0500

    refactor ResultItemComponent to Cards (#485)
    * rename ResultItemComponent to StandardCardComponent
    following commits will change implementation
    
    * add parent card component
    
    * use data-prop isntead of shoving data into json
    
    * change addChild to allow passing methods into child config without accessing _parentOpts,
    add template mappings to cardcomponent's config
    
    * registry.js organization, semistandard fix
    
    * use data-opts, remove template mappings
    
    * address tom's comments
    
    * add missing data prop

---
+++ src/ui/components/cards/consts.js
@@ -0,0 +1,7 @@
+export const cardTemplates = {
+  'Standard': 'cards/standard'
+};
+
+export const cardTypes = {
+  'Standard': 'StandardCard'
+};
---
+++ src/ui/components/cards/standardcardcomponent.js
@@ -0,0 +1,75 @@
+/** @module StandardCardComponent */
+
+import Component from '../component';
+import { cardTemplates, cardTypes } from './consts';
+
+export default class StandardCardComponent extends Component {
+  constructor (config = {}, systemConfig = {}) {
+    super(config, systemConfig);
+
+    /**
+     * verticalConfigId used for analytics
+     * @type {string}
+     * @private
+     */
+    this.verticalConfigId = config.verticalConfigId;
+
+    /**
+     * isUniversal is used for analytics and is set to
+     * true if this component is added by the UniversalResultsComponent
+     * @type {boolean}
+     * @private
+     */
+    this.isUniversal = config.isUniversal || false;
+  }
+
+  static get type () {
+    return cardTypes.Standard;
+  }
+
+  /**
+   * The template to render
+   * @returns {string}
+   * @override
+   */
+  static defaultTemplateName (config) {
+    return cardTemplates.Standard;
+  }
+
+  static areDuplicateNamesAllowed () {
+    return true;
+  }
+
+  /**
+   * helper to construct the eventOptions object for the title link
+   * @param entityId The ID of the result item, if present
+   * @param url The url of the result item, if present
+   * @returns {string}
+   */
+  eventOptions (entityId, url) {
+    const options = {
+      verticalConfigId: this._config.verticalConfigId,
+      searcher: this._config.isUniversal ? 'UNIVERSAL' : 'VERTICAL'
+    };
+
+    if (entityId) {
+      options.entityId = entityId;
+    } else {
+      options.url = url;
+    }
+
+    return JSON.stringify(options);
+  }
+
+  /**
+   * passes eventOptions to the viewModel
+   * @override
+   * @param data
+   * @returns {StandardCardComponent}
+   */
+  setState (data) {
+    return super.setState(Object.assign(data, {
+      eventOptions: this.eventOptions(data.id, data.link)
+    }));
+  }
+}
