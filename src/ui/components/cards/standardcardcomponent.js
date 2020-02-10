35fe7bb9bcab69b3439eb25d3953c7810e73467d
Oliver Shi <oshi@yext.com>
2020-02-10 14:40:26 -0500

    StandardCard Styling (#487)
    * StandardCard Styling
    
    This card implements new styling as well as templateMappings config option
    for vertical results cards.
    
    TEST=manual

--- src/ui/components/cards/standardcardcomponent.js
+++ src/ui/components/cards/standardcardcomponent.js
@@ -2,74 +2,130 @@
 
 import Component from '../component';
 import { cardTemplates, cardTypes } from './consts';
+import DOM from '../../dom/dom';
 
+class StandardCardConfig {
+  constructor (config = {}) {
+    Object.assign(this, config);
+
+    /**
+     * The result data
+     * @type {Result}
+     */
+    const result = config.data || {};
+    const templateMappings = config.templateMappings || (() => {});
+    Object.assign(this, templateMappings(result));
+
+    /**
+     * The raw profile data
+     * @type {Object}
+     */
+    const rawResult = result._raw || {};
+
+    /**
+     * Title for the card
+     * @type {string}
+     */
+    this.title = this.title || result.title || rawResult.name || '';
+
+    /**
+     * Details for the card
+     * @type {string}
+     */
+    this.details = this.details || result.details || rawResult.description || '';
+
+    /**
+     * Url when you click the title
+     * @type {string}
+     */
+    this.url = this.url || result.link || rawResult.website;
+
+    /**
+     * If showMoreLimit is set, the text that displays beneath it
+     * @type {string}
+     */
+    this.showMoreText = this.showMoreText || 'Show More';
+
+    /**
+     * If showMoreLimit is set, the text that displays beneath it when all text is shown
+     * @type {string}
+     */
+    this.showLessText = this.showLessText || 'Show Less';
+
+    /**
+     * Add a show more link if this number of characters is shown,
+     * and truncate the last 3 characters with an ellipses.
+     * Clicking show more should expand the results (but no “show less” link).
+     * @type {number}
+     */
+    this.showMoreLimit = this.showMoreLimit || 350;
+
+    /**
+     * Whether the click should open in a new window
+     * @type {boolean}
+     */
+    this.newWindow = this.newWindow;
+
+    /**
+     * Image url to display
+     * @type {string}
+     */
+    this.image = this.image;
+
+    /**
+     * Subtitle
+     * @type {string}
+     */
+    this.subtitle = this.subtitle;
+
+    /**
+     * Whether a 'show more' toggle button needs to be rendered at all
+     */
+    const detailsOverLimit = this.details.length > this.showMoreLimit;
+    this.showToggle = this.showMoreLimit && detailsOverLimit;
+  }
+}
+
+/**
+ * Card components expect to receive a data config option, containing data regarding entity result
+ * each card is assigned to, including all field data in data._raw.
+ */
 export default class StandardCardComponent extends Component {
   constructor (config = {}, systemConfig = {}) {
-    super(config, systemConfig);
+    super(new StandardCardConfig(config), systemConfig);
+    this.hideExcessDetails = this._config.showToggle;
+  }
 
-    /**
-     * verticalConfigId used for analytics
-     * @type {string}
-     * @private
-     */
-    this.verticalConfigId = config.verticalConfigId;
+  setState (data) {
+    const details = this.hideExcessDetails
+      ? `${this._config.details.substring(0, this._config.showMoreLimit)}...`
+      : this._config.details;
+    return super.setState({
+      ...data,
+      hideExcessDetails: this.hideExcessDetails,
+      details
+    });
+  }
 
-    /**
-     * isUniversal is used for analytics and is set to
-     * true if this component is added by the UniversalResultsComponent
-     * @type {boolean}
-     * @private
-     */
-    this.isUniversal = config.isUniversal || false;
+  onMount () {
+    if (this._config.showToggle) {
+      const el = DOM.query(this._container, '.yxt-StandardCard-toggle');
+      DOM.on(el, 'click', () => {
+        this.hideExcessDetails = !this.hideExcessDetails;
+        this.setState();
+      });
+    }
   }
 
   static get type () {
     return cardTypes.Standard;
   }
 
-  /**
-   * The template to render
-   * @returns {string}
-   * @override
-   */
-  static defaultTemplateName (config) {
+  static defaultTemplateName () {
     return cardTemplates.Standard;
   }
 
   static areDuplicateNamesAllowed () {
     return true;
-  }
-
-  /**
-   * helper to construct the eventOptions object for the title link
-   * @param entityId The ID of the result item, if present
-   * @param url The url of the result item, if present
-   * @returns {string}
-   */
-  eventOptions (entityId, url) {
-    const options = {
-      verticalConfigId: this._config.verticalConfigId,
-      searcher: this._config.isUniversal ? 'UNIVERSAL' : 'VERTICAL'
-    };
-
-    if (entityId) {
-      options.entityId = entityId;
-    } else {
-      options.url = url;
-    }
-
-    return JSON.stringify(options);
-  }
-
-  /**
-   * passes eventOptions to the viewModel
-   * @override
-   * @param data
-   * @returns {StandardCardComponent}
-   */
-  setState (data) {
-    return super.setState(Object.assign(data, {
-      eventOptions: this.eventOptions(data.id, data.link)
-    }));
   }
 }
