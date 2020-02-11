ceb4662b5a56a17aa7ee61a324b8790cdd56627b
Oliver Shi <oshi@yext.com>
2020-02-11 17:51:59 -0500

    tom's elegant resolveCTAMapping

--- src/ui/components/ctas/ctacollectioncomponent.js
+++ src/ui/components/ctas/ctacollectioncomponent.js
@@ -19,46 +19,44 @@ export default class CTACollectionComponent extends Component {
      */
     let callsToAction = this._config.callsToAction || [];
 
-    if (typeof callsToAction === 'function') {
-      callsToAction = callsToAction(this._config.result) || [];
-    }
-    callsToAction = callsToAction.map(ctaMapping =>
-      this.resolveCTAMapping(ctaMapping, this._config.result)
-    );
-
     /**
      * The computed calls to action array
      * @type {Array<Object>}
      */
-    this.callsToAction = callsToAction;
+    this.callsToAction = this.resolveCTAMapping(callsToAction, this._config.result);
   }
 
   /**
    * Handles resolving ctas from a cta mapping which is either
    * 1. a function that returns a cta's config
-   * 2. an object that has a per-attribute mapping of either a
+   * 3. an object that has a per-attribute mapping of either a
    *    a) static value
    *    b) function that takes in resut data and returns the given attributes value
-   * 3. an api field name that keys into the result data which contains the cta config as a json string
-   * @param {Function|Object|string} ctaMapping
+   * 4. an api field name that keys into the result data which contains the cta config as a json string
+   * @param {Function|Array<Object|string>|Object|string} ctaMapping
    * @param {Object} result
-   * @returns {Object}
+   * @returns {Array<Object>}
    */
   resolveCTAMapping (ctaMapping, result) {
     if (typeof ctaMapping === 'function') {
-      return ctaMapping(result);
-    } else if (typeof ctaMapping === 'object') {
+      return this.resolveCTAMapping(ctaMapping(result), result);
+    }
+    if (Array.isArray(ctaMapping)) {
+      return ctaMapping.flatMap(cta => this.resolveCTAMapping(cta, result));
+    }
+    if (typeof ctaMapping === 'object') {
       const ctaObject = { ...ctaMapping };
       for (let [ctaAttribute, attributeMapping] of Object.entries(ctaMapping)) {
         if (typeof attributeMapping === 'function') {
           ctaObject[ctaAttribute] = attributeMapping(result);
         }
       }
-      return ctaObject;
-    } else if (typeof cta === 'string') {
-      return { ...JSON.parse(result[ctaMapping]) };
+      return [ ctaObject ];
     }
-    return {};
+    if (typeof cta === 'string') {
+      return [{ ...JSON.parse(result[ctaMapping]) }];
+    }
+    return [];
   }
 
   setState (data) {
