c70892af379d8cdde33caf5c47eb335345c436f5
Tom Meyer <tmeyer@yext.com>
2020-02-27 11:19:15 -0500

    Remove templateUrl option from the SDK.
    This PR removes the templateUrl option from the SDK. This functionality led
    to a security vulnerability as we were dynamically adding a <script> tag
    to the page with whatever the templateUrl was. With this change, a client is
    still able to supply a custom bundle to Answers, but it must be done using the
    templateBundle attribute. If no templateBundle is supplied, we will fetch the
    default set of templates for the current release.
    
    TEST=manual
    
    Made sure that I could supply a custom bundle via templateBundle. Also made
    sure that if I did not supply a custom bundle, the default was fetched from
    the CDN.

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,4 +4,4 @@ export { default as DOM } from './dom/dom';
 export { default as SearchParams } from './dom/searchparams';
 
 export { Renderers } from './rendering/const';
-export { default as TemplateLoader } from './rendering/templateloader';
+export { default as DefaultTemplatesLoader } from './rendering/defaulttemplatesloader';
