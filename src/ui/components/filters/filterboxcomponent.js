cacc628e560caec3d4d655e23f6a9a5393db1dc8
Oliver Shi <oshi@yext.com>
2020-03-05 14:50:44 -0500

    consolidate filter/facet/sorting getstate logic for vertical search (#504)
    There is a lot of repeated pre- 'core.verticalSearch()' code
    that makes it really annoying to change how filters work,
    and doesn't really make sense to be repeated.
    
    The steps are basically:
    1. get ALL filters
    2. AND them together if there is more than one
    3. Also get the facet filter in storage (of which we only expect one)
    
    TEST=manual
    Created test site with geolocationfilter, daterangefilter, sortoptions,
    filteroptions, filterbox, facets, and vertical results + searchbar.
    Tested that individual filters worked properly. There are still
    issues with instantiating filters from persistent storage and
    how clicking apply on one filter will not also apply any other filter
    changes selected, however these issues were present prior to this
    change

--- src/ui/components/filters/filterboxcomponent.js
+++ src/ui/components/filters/filterboxcomponent.js
@@ -285,21 +285,9 @@ export default class FilterBoxComponent extends Component {
    * Trigger a search with all filters in storage
    */
   _search () {
-    const allFilters = this.core.globalStorage.getAll(StorageKeys.FILTER);
-    const totalFilter = allFilters.length > 1
-      ? Filter.and(...allFilters)
-      : allFilters[0];
-
     const query = this.core.globalStorage.getState(StorageKeys.QUERY);
-
-    const facetFilter = this.core.globalStorage.getAll(StorageKeys.FACET_FILTER)[0];
-
     this.core.persistentStorage.delete(StorageKeys.SEARCH_OFFSET);
     this.core.globalStorage.delete(StorageKeys.SEARCH_OFFSET);
-    this.core.verticalSearch(this._verticalKey, {
-      input: query,
-      filter: JSON.stringify(totalFilter),
-      facetFilter: JSON.stringify(facetFilter)
-    });
+    this.core.verticalSearch(this._verticalKey, { input: query });
   }
 }
