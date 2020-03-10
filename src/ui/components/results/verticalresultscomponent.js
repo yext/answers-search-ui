37afd6286a9e39b0fd65aa7579ad5d558e43605e
Oliver Shi <oshi@yext.com>
2020-03-10 16:51:12 -0400

    Fix max number of columns on init: Figured out a way to do card columns without a dom query

--- src/ui/components/results/verticalresultscomponent.js
+++ src/ui/components/results/verticalresultscomponent.js
@@ -9,19 +9,6 @@ import StorageKeys from '../../../core/storage/storagekeys';
 import SearchStates from '../../../core/storage/searchstates';
 import CardComponent from '../cards/cardcomponent';
 import ResultsHeaderComponent from './resultsheadercomponent';
-import DOM from '../../dom/dom';
-
-/**
- * Breakpoint for when somebody is on desktop, prescribed by Jeremy.
- * @type {number}
- */
-const DESKTOP_BREAKPOINT = 400;
-
-/**
- * Minimum width for the card.
- * @type {number}
- */
-const CARD_MIN_WIDTH = 210;
 
 class VerticalResultsConfig {
   constructor (config = {}) {
@@ -137,8 +124,7 @@ export default class VerticalResultsComponent extends Component {
      * @type {Array<Result>}
      */
     this.results = [];
-    this.numColumns = 1;
-    this.handleResize = this.handleResize.bind(this);
+    this.numColumns = this._config.maxNumberOfColumns;
   }
 
   mount () {
@@ -150,54 +136,6 @@ export default class VerticalResultsComponent extends Component {
 
   static get duplicatesAllowed () {
     return true;
-  }
-
-  /**
-   * Gets the number of columns needed, capped at the max number of columns
-   * set in the config, and also the number of results.
-   * @returns {number}
-   */
-  getNumColumns () {
-    if (this._config.maxNumberOfColumns <= 1 || !this.results.length) {
-      return 1;
-    }
-    const totalWidth = this._container.scrollWidth;
-    if (totalWidth <= DESKTOP_BREAKPOINT) {
-      return 1;
-    }
-    const numColumns = Math.floor(totalWidth / CARD_MIN_WIDTH);
-    return Math.min(numColumns, this._config.maxNumberOfColumns, this.results.length);
-  }
-
-  handleResize () {
-    if (this._handleResizeTimer) {
-      clearTimeout(this._handleResizeTimer);
-    }
-
-    this._handleResizeTimer = setTimeout(() => {
-      const currentNumColumns = this.getNumColumns();
-      if (this.numColumns !== currentNumColumns) {
-        this.numColumns = currentNumColumns;
-        const resultsEl = DOM.query(this._container, '.yxt-Results-items');
-        resultsEl.classList.remove('yxt-Results-items--1');
-        resultsEl.classList.remove('yxt-Results-items--2');
-        resultsEl.classList.remove('yxt-Results-items--3');
-        resultsEl.classList.remove('yxt-Results-items--4');
-        resultsEl.classList.add(`yxt-Results-items--${currentNumColumns}`);
-      }
-    }, 100);
-  }
-
-  onCreate () {
-    if (this._config.maxNumberOfColumns > 1) {
-      window.addEventListener('resize', this.handleResize);
-    }
-  }
-
-  onDestroy () {
-    if (this._config.maxNumberOfColumns > 1) {
-      window.removeEventListener('resize', this.handleResize);
-    }
   }
 
   setState (data, val) {
@@ -212,7 +150,6 @@ export default class VerticalResultsComponent extends Component {
     const displayResultsIfExist = this._config.isUniversal ||
       this._config._displayAllResults ||
       data.resultsContext === ResultsContext.NORMAL;
-    this.numColumns = this.getNumColumns();
     const showResultsHeader = this._config.resultsHeaderOpts.showResultsCount ||
       this._config.resultsHeaderOpts.showAppliedFilters;
 
@@ -230,7 +167,7 @@ export default class VerticalResultsComponent extends Component {
       showNoResults: data.resultsContext === ResultsContext.NO_RESULTS,
       isEnhancedNoResultsEnabled: this._config._showEnhancedNoResults,
       placeholders: new Array(this._config.maxNumberOfColumns - 1),
-      numColumns: this.numColumns,
+      numColumns: Math.min(this._config.maxNumberOfColumns, this.results.length),
       showResultsHeader: showResultsHeader
     }), val);
   }
