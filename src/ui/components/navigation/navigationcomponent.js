c246567e710f3be5906bc7201e04e658794b3cc9
Alexis Grow <agrow@yext.com>
2020-03-23 10:09:47 -0400

    PR fixes

--- src/ui/components/navigation/navigationcomponent.js
+++ src/ui/components/navigation/navigationcomponent.js
@@ -99,7 +99,7 @@ export class Tab {
 
       // For tabs without config ids, map their URL to the configID
       // to avoid duplication of renders
-      if (!tab.verticalKey && !tabs[tab.verticalKey]) {
+      if (!tab.verticalKey && !tabs[tab.url]) {
         tab.verticalKey = tab.url;
       }
 
