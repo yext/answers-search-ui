a80689b3cef4972df1df0e386299868a00edc0e8
Alexis Grow <39104038+alexisgrow@users.noreply.github.com>
2020-03-26 20:16:20 -0400

    Cards: Update details null checking from undefined to null (#592)

--- src/ui/components/cards/standardcardcomponent.js
+++ src/ui/components/cards/standardcardcomponent.js
@@ -47,7 +47,7 @@ class StandardCardConfig {
      * Details for the card
      * @type {string}
      */
-    this.details = this.details === undefined ? '' : (this.details || result.details || rawResult.description || '');
+    this.details = this.details === null ? null : (this.details || result.details || rawResult.description || '');
 
     /**
      * Url when you click the title
