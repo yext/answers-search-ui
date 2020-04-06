f29691aaf7822d10742193037665f330c48beb89
Oliver Shi <oshi@yext.com>
2020-04-06 17:49:51 -0400

    Accordion 13.2 qa (#655)
    * Accordion Card 13.2 QA:
    - improve animation to match AccordionResult, by adding a container around contents
    - add/move padding around to better match AccordionResult and mocks
    
    TEST=manual
    load page with AccordionCard, test animation, inspect DOM for padding and bold title

--- src/ui/components/cards/accordioncardcomponent.js
+++ src/ui/components/cards/accordioncardcomponent.js
@@ -122,17 +122,17 @@ export default class AccordionCardComponent extends Component {
    * it doesn't rip the whole component off of the page and remount it.
    * Also reports an analytics event.
    * @param {HTMLElement} toggleEl the toggle element
-   * @param {HTMLElement} contentEl the content element
+   * @param {HTMLElement} accordionBodyEl the .js-yxt-AccordionCard-body element
    * @param {HTMLElement} accordionEl the root accordion element
    */
-  handleClick (toggleEl, contentEl, accordionEl) {
+  handleClick (toggleEl, accordionBodyEl, accordionEl) {
     this.isExpanded = !this.isExpanded;
     accordionEl.classList.toggle('yxt-AccordionCard--expanded');
 
-    contentEl.style.height = `${this.isExpanded ? contentEl.scrollHeight : 0}px`;
+    accordionBodyEl.style.height = `${this.isExpanded ? accordionBodyEl.scrollHeight : 0}px`;
 
     toggleEl.setAttribute('aria-expanded', this.isExpanded ? 'true' : 'false');
-    contentEl.setAttribute('aria-hidden', this.isExpanded ? 'false' : 'true');
+    accordionBodyEl.setAttribute('aria-hidden', this.isExpanded ? 'false' : 'true');
     const event = new AnalyticsEvent(this.isExpanded ? 'ROW_EXPAND' : 'ROW_COLLAPSE')
       .addOptions({
         verticalKey: this.verticalKey,
@@ -144,12 +144,12 @@ export default class AccordionCardComponent extends Component {
 
   onMount () {
     const toggleEl = DOM.query(this._container, '.js-yxt-AccordionCard-toggle');
-    const contentEl = DOM.query(this._container, '.js-yxt-AccordionCard-content');
+    const accordionBodyEl = DOM.query(this._container, '.js-yxt-AccordionCard-body');
     const accordionEl = DOM.query(this._container, '.js-yxt-AccordionCard');
 
-    contentEl.style.height = `${this.isExpanded ? contentEl.scrollHeight : 0}px`;
+    accordionBodyEl.style.height = `${this.isExpanded ? accordionBodyEl.scrollHeight : 0}px`;
 
-    DOM.on(toggleEl, 'click', () => this.handleClick(toggleEl, contentEl, accordionEl));
+    DOM.on(toggleEl, 'click', () => this.handleClick(toggleEl, accordionBodyEl, accordionEl));
   }
 
   /**
