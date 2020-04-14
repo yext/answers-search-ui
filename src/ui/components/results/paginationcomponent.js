9b638e6d0ecaf80652aa008260dc62b4c0f4210e
Oliver Shi <oshi@yext.com>
2020-04-14 09:42:42 -0400

    Component: consolidate vertical search logic into component.js (#639)
    * Component: consolidate vertical search logic
    
    This commit moves lots of repeated logic for vertical search into
    core.verticalSearch, and represents the more conditional logic
    (whether to delete the SEARCH_OFFSET or whether to include facets
    in the search) in an options parameter (inspired by nodejs)
    
    TEST=manual
    Created test page with Facets, SortOptions, LocationBias, GeoLocationFilter,
    and FilterSearch.
    For Facets, FilterSearch, SortOptions, GeoLocationFilter, tested that
      changing their values would affect the search, and refreshing the page
      kept these values and also applied them to the initial search
    For LocationBias, checked that clicking "Update my location" would
      refresh the search, with location data added to the query

--- src/ui/components/results/paginationcomponent.js
+++ src/ui/components/results/paginationcomponent.js
@@ -187,7 +187,7 @@ export default class PaginationComponent extends Component {
     this._onPaginate(newPageNumber, currentPageNumber, maxPageCount);
     this.core.globalStorage.set(StorageKeys.SEARCH_OFFSET, offset);
     this.core.persistentStorage.set(StorageKeys.SEARCH_OFFSET, offset);
-    this.core.verticalPage(this._verticalKey, offset);
+    this.core.verticalPage(this._verticalKey);
   }
 
   scrollToTop () {
