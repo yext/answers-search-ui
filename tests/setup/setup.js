5df1bd258eb179d6becc2ce8dc32dc905124e73a
Oliver Shi <oshi@yext.com>
2020-04-14 09:41:16 -0400

    Add FilterRegistry, FilterNode, FilterView and supporting classes (#672)
    * Add FilterRegistry and FilterNode infrastructure
    This commit adds 
    1. FilterRegistry, a class for filter management
    2. FilterNode (and its two implementations CombinedFIlterNode and SimpleFilterNode)
    3. FilterNodeFactory, a class of static FilterNode builders
    will be a new way to store filters/facets in the SDK, and replace our
    current usage of global storage (only for filters/facets).
    4. FilterMetadata, a class for holding additional display data for a filter
    5. FilterCombinators, an enum for the ways to combine filters (currently $and and $or)
    
    This should allow us to store additional metadata and callbacks
    next to a filter, without sacrificing information on how the
    filter was originally constructed.
    
    TEST=auto
    wrote unit tests for FilterRegistry and FilterNodeFactory
    testing basic filter nodes, as well as more nested cases.

--- tests/setup/setup.js
+++ tests/setup/setup.js
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import AnswersAdapter from './enzymeadapter';
+import flatMap from 'array.prototype.flatmap';
 
 configure({ adapter: new AnswersAdapter() });
+flatMap.shim();
