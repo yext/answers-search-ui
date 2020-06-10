e007e3bfa199ffb1c990b922d0de08191c9c1ad5
Oliver Shi <oshi@yext.com>
2020-06-10 11:57:48 -0400

    Revert "fix entityType reference"
    This reverts commit e73befe4637d5ce48ee7a4c79b09dddf8ad87879.

--- tests/core/utils/configutils.js
+++ tests/core/utils/configutils.js
@@ -1,6 +1,6 @@
-import { getOrDefault } from 'src/core/utils/configutils.js';
+import { defaultConfigOption } from 'src/core/utils/configutils.js';
 
-describe('getOrDefault helper method', () => {
+describe('defaultConfigOption helper method', () => {
   it('works for nested config', () => {
     const testConfig = {
       a: {
@@ -9,7 +9,7 @@ describe('getOrDefault helper method', () => {
         }
       }
     };
-    expect(getOrDefault(testConfig, ['a.b.c'], 'default')).toEqual(1234);
+    expect(defaultConfigOption(testConfig, ['a.b.c'], 'default')).toEqual(1234);
   });
 
   it('defaults when config option is not found', () => {
@@ -20,7 +20,7 @@ describe('getOrDefault helper method', () => {
         }
       }
     };
-    expect(getOrDefault(testConfig, ['a.b.c'], 'default')).toEqual('default');
+    expect(defaultConfigOption(testConfig, ['a.b.c'], 'default')).toEqual('default');
   });
 
   it('works for multiple config synonyms', () => {
@@ -32,6 +32,6 @@ describe('getOrDefault helper method', () => {
       },
       LOL: 'not me'
     };
-    expect(getOrDefault(testConfig, ['a.b.c', 'a.b.LOL', 'LOL', 'default'], 'default')).toEqual('yes');
+    expect(defaultConfigOption(testConfig, ['a.b.c', 'a.b.LOL', 'LOL', 'default'], 'default')).toEqual('yes');
   });
 });
