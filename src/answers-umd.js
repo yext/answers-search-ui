43a208faa876d6bd1932245dd5cc527633215434
creotutar <creotutar@yext.com>
2020-06-16 18:47:47 -0400

    Surround in a try-catch, do not block js if URL is relative

--- src/answers-umd.js
+++ src/answers-umd.js
@@ -426,11 +426,15 @@ class Answers {
       onSuccess: config.onSuccess || function () {},
       onFinally: config.onFinally || function () {},
       onBeforeSend: (xhr, node, url) => {
-        const uriWithCacheBust = new URL(url);
-        const params = new SearchParams(uriWithCacheBust.search);
-        params.set('_', new Date().getTime());
-        uriWithCacheBust.search = params.toString();
-        xhr.open('GET', uriWithCacheBust.toString());
+        try {
+          const uriWithCacheBust = new URL(url);
+          const params = new SearchParams(uriWithCacheBust.search);
+          params.set('_', new Date().getTime());
+          uriWithCacheBust.search = params.toString();
+          xhr.open('GET', uriWithCacheBust.toString());
+        } catch (e) {
+          // Catch the error and continue if the URL provided in the asset is not a valid URL
+        }
       }
     });
   }
