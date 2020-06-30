4a437f8b31567e1e79701b47a63ed42bd1dcaebc
Sara Wong <sawong@yext.com>
2020-06-30 22:45:58 +0800

    Update behavior for empty search when redirect url is provided

--- src/ui/components/search/searchcomponent.js
+++ src/ui/components/search/searchcomponent.js
@@ -413,12 +413,14 @@ export default class SearchComponent extends Component {
 
     // If we have a redirectUrl, we want the form to be
     // serialized and submitted.
-    if (typeof this.redirectUrl === 'string') {
-      if (params.has(StorageKeys.REFERRER_PAGE_URL)) {
-        params.delete(StorageKeys.REFERRER_PAGE_URL);
+    if (typeof this.redirectUrl === 'string' && this._verticalKey) {
+      if (this._allowEmptySearch || query) {
+        if (params.has(StorageKeys.REFERRER_PAGE_URL)) {
+          params.delete(StorageKeys.REFERRER_PAGE_URL);
+        }
+        window.location.href = this.redirectUrl + '?' + params.toString();
+        return false;
       }
-      window.location.href = this.redirectUrl + '?' + params.toString();
-      return false;
     }
 
     inputEl.blur();
