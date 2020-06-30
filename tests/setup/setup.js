ecb578e8315c1768fd0625b1f4d9f7a04bfd44f4
Oliver Shi <oshi@yext.com>
2020-06-30 18:32:59 -0400

    Update circleci node version from 7.10 to 14.5
    removed jest test polyfills for 7.10
    
    added npm rebuild node-sass to circle ci npm install step,
    because circle ci was complaining about needing node-sass bindings
    fo4 14.x but finding bindings for 7.x on the machine.
    I experienced this locally as well.
    
    TEST=manual
    ran acceptance/unit tests
    did basic vertical/universal search flow (can search, filter/apply facets,
      use qa submission, direct answers, analytics events fire, spellcheck works)

--- tests/setup/setup.js
+++ tests/setup/setup.js
@@ -1,8 +1,4 @@
 import { configure } from 'enzyme';
 import AnswersAdapter from './enzymeadapter';
-import flatMap from 'array.prototype.flatmap';
-import padStart from 'string.prototype.padstart';
 
 configure({ adapter: new AnswersAdapter() });
-flatMap.shim();
-padStart.shim();
