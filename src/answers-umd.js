caca8ad2f30a22007315de10df94ce90892ce57f
Oliver Shi <oshi@yext.com>
2020-06-30 10:09:22 -0400

    Expose FilterNodeFactory
    test=manual
    tested with bhaines mercy site, that we can manually set Facets on page
    load using FilterNodeFactory

--- src/answers-umd.js
+++ src/answers-umd.js
@@ -32,6 +32,7 @@ import { SANDBOX, PRODUCTION } from './core/constants';
 import MasterSwitchApi from './core/utils/masterswitchapi';
 import RichTextFormatter from './core/utils/richtextformatter';
 import { isValidContext } from './core/utils/apicontext';
+import FilterNodeFactory from './core/filters/filternodefactory';
 
 /** @typedef {import('./core/services/searchservice').default} SearchService */
 /** @typedef {import('./core/services/autocompleteservice').default} AutoCompleteService */
@@ -71,6 +72,11 @@ class Answers {
      * custom analytics
      */
     this.AnalyticsEvent = AnalyticsEvent;
+
+    /**
+     * A reference to the FilterNodeFactory class for creating {@link FilterNode}s.
+     */
+    this.FilterNodeFactory = FilterNodeFactory;
 
     /**
      * A reference of the renderer to use for the components
