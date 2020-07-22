2b4213571d1bbe99753236cff0f515aff23c875f
Sara Wong <sawong@yext.com>
2020-07-22 18:21:56 +0800

    Ensure tab order is preserved when clicking View All on the universal page

--- src/ui/components/results/verticalresultscomponent.js
+++ src/ui/components/results/verticalresultscomponent.js
@@ -12,6 +12,7 @@ import ResultsHeaderComponent from './resultsheadercomponent';
 import { addParamsToUrl } from '../../../core/utils/urlutils';
 import Icons from '../../icons/index';
 import { defaultConfigOption } from '../../../core/utils/configutils';
+import SearchParams from '../../dom/searchparams';
 
 class VerticalResultsConfig {
   constructor (config = {}) {
@@ -242,19 +243,87 @@ export default class VerticalResultsComponent extends Component {
     return addParamsToUrl(universalConfig.url, params);
   }
 
+  getUrlParams () {
+    return new SearchParams(window.location.search.substring(1));
+  }
+
+  getDefaultTabOrder (tabsConfig, urlParams) {
+    let tabOrder = [];
+
+    // Use the ordering from the URL as the primary configuration
+    // And then merge it with the local configuration, if provided.
+    if (urlParams && urlParams.has('tabOrder')) {
+      tabOrder = urlParams.get('tabOrder').split(',');
+    }
+
+    for (let i = 0; i < tabsConfig.length; i++) {
+      const tab = tabsConfig[i];
+      // Some tabs don't have verticalKey, so we map it from URL
+      if (!tab.verticalKey) {
+        tab.verticalKey = tab.url;
+      }
+      // Avoid duplicates if config was provided from URL
+      if (tabOrder.includes(tab.verticalKey)) {
+        continue;
+      }
+
+      // isFirst should always be the first element in the list
+      if (tab.isFirst) {
+        tabOrder.unshift(tab.verticalKey);
+      } else {
+        tabOrder.push(tab.verticalKey);
+      }
+    }
+    return tabOrder;
+  }
+
+  mergeTabOrder (tabOrder, otherTabOrder) {
+    for (let i = 0; i < otherTabOrder.length; i++) {
+      const tabConfig = otherTabOrder[i];
+      if (tabOrder.includes(tabConfig)) {
+        continue;
+      }
+      // isFirst should be an override to dynamic tab ordering.
+      if (this._verticalsConfig[tabConfig] && this._verticalsConfig[tabConfig].isFirst) {
+        tabOrder.unshift(tabConfig);
+      } else {
+        tabOrder.push(tabConfig);
+      }
+    }
+
+    return tabOrder;
+  }
+
+  generateTabUrl (baseUrl, params) {
+    const context = this.core.globalStorage.getState(StorageKeys.API_CONTEXT);
+    if (context) {
+      params.set(StorageKeys.API_CONTEXT, context);
+    }
+    const referrerPageUrl = this.core.globalStorage.getState(StorageKeys.REFERRER_PAGE_URL);
+    if (referrerPageUrl !== null) {
+      params.set(StorageKeys.REFERRER_PAGE_URL, referrerPageUrl);
+    }
+
+    // We want to persist the params from the existing URL to the new
+    // URLS we create.
+    let dataTabOrder = [];
+    let tabOrder = this.getDefaultTabOrder(this._verticalsConfig, this.getUrlParams());
+    if (this.core.globalStorage.getState(StorageKeys.NAVIGATION)) {
+      dataTabOrder = this.core.globalStorage.getState(StorageKeys.NAVIGATION).tabOrder;
+    }
+    if (tabOrder !== undefined || dataTabOrder !== undefined) {
+      tabOrder = this.mergeTabOrder(dataTabOrder, tabOrder);
+      params.set('tabOrder', tabOrder);
+    }
+
+    return baseUrl + '?' + params.toString();
+  }
+
   getVerticalURL (data = {}) {
     const verticalConfig = this._verticalsConfig.find(config => config.verticalKey === this.verticalKey) || {};
     const verticalURL = this._config.verticalURL || verticalConfig.url || data.verticalURL || this.verticalKey + '.html';
 
-    const params = {
-      query: this.query,
-      referrerPageUrl: this.core.globalStorage.getState(StorageKeys.REFERRER_PAGE_URL)
-    };
-    const context = this.core.globalStorage.getState(StorageKeys.API_CONTEXT);
-    if (context) {
-      params.context = context;
-    }
-    return addParamsToUrl(verticalURL, params);
+    return this.generateTabUrl(verticalURL, this.getUrlParams());
   }
 
   setState (data = {}, val) {
