734d3e3e99a48b1e217f9eb623a778a23be23910
Oliver Shi <oshi@yext.com>
2020-08-19 17:54:21 -0400

    translator.js: handle multiple plural forms (#1033)
    * translator.js: handle multiple plural forms
    
    update translate helpers to reflect changes
    from https://github.com/yext/jambo/pull/85
    
    J=SLAP-575
    TEST=auto

--- src/core/i18n/translator.js
+++ src/core/i18n/translator.js
@@ -1,3 +1,5 @@
+import { getNPlurals, getPluralFunc, hasLang } from 'plural-forms/dist/minimal';
+
 export default class Translator {
   /**
    * Performs a translation which supports
@@ -7,17 +9,49 @@ export default class Translator {
    * @param {number} count The count associated with the pluralization
    */
   static translate (translations, interpolationParams, count) {
-    const stringToInterpolate = this._selectPluralization(translations, count);
+    const stringToInterpolate = this._selectStringToInterpolate(translations, count);
     return this._interpolate(stringToInterpolate, interpolationParams);
   }
 
-  static _selectPluralization (translations, count) {
+  /**
+   * If translations is json, parse it and choose the correct plural form.
+   * Otherwise just return the non-interpolated translation string.
+   * @param {string} translations
+   * @param {number} count
+   * @returns {string}
+   */
+  static _selectStringToInterpolate (translations, count) {
     try {
       translations = JSON.parse(translations);
-      return count > 1 ? translations.plural : translations['1'];
     } catch (e) {
       return translations;
     }
+    return this._selectPluralForm(translations, count);
+  }
+
+  /**
+   * Returns the correct plural form given a parsed translations object and count.
+   * @param {Object} parsedTranslations
+   * @param {number} count
+   * @returns {string}
+   */
+  static _selectPluralForm (parsedTranslations, count) {
+    let locale = parsedTranslations.locale;
+    if (!hasLang(locale)) {
+      locale = 'en';
+    }
+    const oneToNArray = this._generateArrayOneToN(locale);
+    const pluralFormIndex = getPluralFunc(locale)(count, oneToNArray);
+    return parsedTranslations[pluralFormIndex];
+  }
+
+  /**
+   * @param {string} locale
+   * @returns {Array} an array of the form [0, 1, 2, ..., nPluralForms]
+   */
+  static _generateArrayOneToN (locale) {
+    const numberOfPluralForms = getNPlurals(locale);
+    return Array.from((new Array(numberOfPluralForms)).keys());
   }
 
   static _interpolate (stringToInterpolate, interpolationParams) {
