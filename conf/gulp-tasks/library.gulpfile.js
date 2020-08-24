1a2e35fbedc1fff57b0321e83bf4b4f0efabcae6
Tom Meyer <tmeyer@yext.com>
2020-08-24 17:10:51 -0400

    Create a BundleTaskFactory class that provides Gulp tasks for different SDK bundles (#1038)
    This PR adds the BundleTaskFactory class. This class provides Gulp tasks for the different types of SDK bundle: modern, legacy-iife, and legacy-umd. With this class, we can simplify the library.gulpfile.js file. That file was getting too bulky and the work required for creating translated SDK bundles would have only made this worse.
    
    TEST=manual
    
    For the same ref, built all the SDK assets with and without my changes. Using a diff, ensured that the generated assets were the same in both cases.

--- conf/gulp-tasks/library.gulpfile.js
+++ conf/gulp-tasks/library.gulpfile.js
@@ -1,131 +1,25 @@
 const { series, parallel, src, dest, watch } = require('gulp');
 
-const fs = require('fs');
-
-const rollup = require('gulp-rollup-lightweight');
-const babel = require('rollup-plugin-babel');
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const insert = require('rollup-plugin-insert');
-
-const source = require('vinyl-source-stream');
 const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-
 const sass = require('gulp-sass');
-
 const postcss = require('gulp-postcss');
-
 const uglify = require('gulp-uglify-es').default;
 
-const NAMESPACE = 'ANSWERS';
+const getLibraryVersion = require('./utils/libversion');
+const { BundleType, BundleTaskFactory } = require('./bundle/bundletaskfactory');
 
-function getLibVersion () {
-  try {
-    const insideWorkTree = require('child_process')
-      .execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
-      .toString().trim();
-    if (insideWorkTree === 'true') {
-      return require('child_process')
-        .execSync('git describe --tags')
-        .toString().trim();
-    }
-  } catch (e) {
-    // if above command fails, catch error and continue, as we are not in a git repository
-  }
-
-  console.warn('Warning: Not in a github repository, using default hardcoded library version.');
-  return 'TEST';
-}
+const bundleTaskFactory = new BundleTaskFactory(getLibraryVersion());
 
 function bundle () {
-  return rollup({
-    input: './src/answers-umd.js',
-    output: {
-      format: 'umd',
-      name: NAMESPACE,
-      exports: 'default',
-      sourcemap: true
-    },
-    plugins: [
-      resolve(),
-      commonjs({
-        include: './node_modules/**'
-      }),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        presets: ['@babel/env']
-      })
-    ]
-  })
-    .pipe(source('answers-modern.js'))
-    .pipe(replace('@@LIB_VERSION', getLibVersion()))
-    .pipe(dest('dist'));
+  return bundleTaskFactory.create(BundleType.MODERN)();
 }
 
 function legacyBundleIIFE () {
-  return legacyBundle({
-    format: 'iife',
-    name: NAMESPACE,
-    sourcemap: true
-  },
-  'answers.js'
-  );
+  return bundleTaskFactory.create(BundleType.LEGACY_IIFE)();
 }
 
 function legacyBundleUMD () {
-  return legacyBundle({
-    format: 'umd',
-    name: NAMESPACE,
-    export: 'default',
-    sourcemap: true
-  },
-  'answers-umd.js'
-  );
-}
-
-function legacyBundle (outputConfig, fileName) {
-  return rollup({
-    input: './src/answers-umd.js',
-    output: outputConfig,
-    plugins: [
-      resolve(),
-      insert.prepend(
-        fs.readFileSync('./conf/gulp-tasks/polyfill-prefix.js').toString(),
-        {
-          include: './src/answers-umd.js'
-        }),
-      commonjs({
-        include: './node_modules/**'
-      }),
-      babel({
-        runtimeHelpers: true,
-        babelrc: false,
-        exclude: 'node_modules/**',
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              'loose': true,
-              'modules': false
-            }
-          ]
-        ],
-        plugins: [
-          '@babel/syntax-dynamic-import',
-          ['@babel/plugin-transform-runtime', {
-            'corejs': 3
-          }],
-          '@babel/plugin-transform-arrow-functions',
-          '@babel/plugin-proposal-object-rest-spread'
-        ]
-      })
-    ]
-  })
-    .pipe(source(fileName))
-    .pipe(replace('@@LIB_VERSION', getLibVersion()))
-    .pipe(dest('dist'));
+  return bundleTaskFactory.create(BundleType.LEGACY_UMD)();
 }
 
 function minifyJS () {
