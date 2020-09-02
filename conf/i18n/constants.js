e71e1183fcffd35b46c76297f756313b2780166b
Alexis Grow <agrow@yext.com>
2020-09-02 08:59:00 -0400

    update regex to allow parens

--- conf/i18n/constants.js
+++ conf/i18n/constants.js
@@ -2,4 +2,4 @@ exports.DEV_LOCALE = 'en';
 
 exports.BUILD_LOCALES = ['en'];
 
-exports.TRANSLATION_FLAGGER_REGEX = /TranslationFlagger.flag\([^;]+?\)/g;
+exports.TRANSLATION_FLAGGER_REGEX = /TranslationFlagger.flag\((\s\S)*\{[^;]+?\}(\s\S)*\)/g;
