c518ce98219ff0a7ca2f780a9b24591d3572335d
Christian Reotutar <32468132+creotutar@users.noreply.github.com>
2020-09-04 19:26:06 -0400

    filteroptions: Explicitly pass replaceHistory based on facet update (#1064)
    * filteroptions: Explicitly pass replaceHistory based on load vs selecting facet
    
    Consider the following situation
    
    In State 0
    Land on a vertical page
    In State 1
    Search for "all"
    In State 2
    Select and apply a facet "Product"
    In State 3
    Search for "people" (any query that will give results)
    In State 4
    Back nav in Chrome history
    In State 5
    
    You would expect state 5 === state 3. That is, you would expect state 5
    to have the search "all" and the facet "Product". Instead, state 5 has the search
    "people" and the facet "Product". This is a result of how we update
    facets. When you are searching for "people", the query is updated and
    pushes a state. Then we get the results back from liveapi, get updated
    facets, and push /another/ state with the updated facets. (IE we are
    pushing two states when we make a search when facets are already being
    replaced).
    
    This change makes replaceHistory explicitly set. Whenever a new set of
    facets are mounted to the page (e.g. we get new facets from liveapi, we
    landed on the page), they we replaceHistory. This is with the knowledge
    that a state was already pushed for the search, and we don't want to
    push another state for the updated facets.
    
    J=SLAP-612
    TEST=manual
    
    Test on a local HH theme site and a local raw site referencing a
    local SDK dist
    
    Test the above situation.
    Test that applying a facet still pushes a state.
    Test that you can apply a facet and then apply another facet and have
    two different states.
    Test that landing without any facet parameters replaces history when the
    facet parameters are added. (ie landing on
    http://localhost:5000/public/people.html?query=all). When navigating
    back, you should go to the previous page, not the landing page without
    parameters.
    
    * Only affect Facet component

--- src/ui/components/filters/filteroptionscomponent.js
+++ src/ui/components/filters/filteroptionscomponent.js
@@ -288,7 +288,7 @@ export default class FilterOptionsComponent extends Component {
     this.showMoreState = this.config.showMore;
 
     if (this.config.storeOnChange) {
-      this.apply();
+      this.apply(this.config.isDynamic);
     }
   }
 
@@ -571,7 +571,7 @@ export default class FilterOptionsComponent extends Component {
   updateListeners (alwaysSaveFilterNodes, blockSearchOnChange) {
     const filterNode = this.getFilterNode();
     if (this.config.storeOnChange) {
-      this.apply();
+      this.apply(false);
     }
 
     this.config.onChange(filterNode, alwaysSaveFilterNodes, blockSearchOnChange);
@@ -590,7 +590,12 @@ export default class FilterOptionsComponent extends Component {
     this.updateListeners();
   }
 
-  apply () {
+  /**
+   * Apply filter changes
+   * @param {boolean} replaceHistory Whether we replace or push a new history
+   *                                 state for the associated changes
+   */
+  apply (replaceHistory) {
     switch (this.config.optionType) {
       case OptionTypes.RADIUS_FILTER:
         this.core.setLocationRadiusFilterNode(this.getLocationRadiusFilterNode());
@@ -602,7 +607,7 @@ export default class FilterOptionsComponent extends Component {
         throw new AnswersComponentError(`Unknown optionType ${this.config.optionType}`, 'FilterOptions');
     }
 
-    this.saveSelectedToPersistentStorage();
+    this.saveSelectedToPersistentStorage(replaceHistory);
   }
 
   floatSelected () {
@@ -662,9 +667,15 @@ export default class FilterOptionsComponent extends Component {
 
   /**
    * Saves selected options to persistent storage
+   * @param {boolean} replaceHistory Whether we replace or push a new history
+   *                                 state for the associated changes
    */
-  saveSelectedToPersistentStorage () {
-    this.core.persistentStorage.set(this.name, this.config.options.filter(o => o.selected).map(o => o.label));
+  saveSelectedToPersistentStorage (replaceHistory) {
+    this.core.persistentStorage.set(
+      this.name,
+      this.config.options.filter(o => o.selected).map(o => o.label),
+      replaceHistory || (this.core.persistentStorage.get(this.name) === null)
+    );
   }
 
   /**
