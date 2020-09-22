342e97397b56aeb98bcf0829740b6e419f2bc449
Christian Reotutar <32468132+creotutar@users.noreply.github.com>
2020-09-22 13:18:41 -0400

    performance: Add performance marks at major events (#1065) (#1088)
    We add performance marks for better metrics into the time between the
    initialization of answers and the subsequent major events like the first
    query request sent or the query response rendering. Cherry-picked from
    feature/sept-regression-fixes
    
    J=SLAP-632
    TEST=manual
    
    Test that on a page load you see all nine performance marks in the
    console window.performance.entries() (page with query, depends
    on whether you are searching on a vertical or a universal for the
    four query marks)
    
    yext.answers.initStart
    yext.answers.statusStart
    yext.answers.statusEnd
    yext.answers.ponyfillStart
    yext.answers.ponyfillend
    yext.answers.verticalQueryStart
    yext.answers.verticalQuerySent
    yext.answers.verticalQueryResponseReceived
    yext.answers.verticalQueryResponseRendered
    yext.answers.universalQueryStart
    yext.answers.universalQuerySent
    yext.answers.universalQueryResponseReceived
    yext.answers.universalQueryResponseRendered

--- tests/acceptance/acceptancesuite.js
+++ tests/acceptance/acceptancesuite.js
@@ -180,3 +180,37 @@ test('Facets, pagination, and filters do not persist accross experience links', 
   await t.expect(viewAllLink).contains('referrerPageUrl');
   await verifyCleanLink(viewAllLink);
 });
+
+fixture`Performance marks on search`
+  .before(setupServer)
+  .after(shutdownServer)
+  .page`http://localhost:9999/tests/acceptance/fixtures/html/facets`;
+
+test('window.performance calls are marked for a normal search', async t => {
+  const marksToCheck = [
+    'yext.answers.initStart',
+    'yext.answers.statusStart',
+    'yext.answers.statusEnd',
+    'yext.answers.ponyfillStart',
+    'yext.answers.ponyfillEnd',
+    'yext.answers.verticalQueryStart',
+    'yext.answers.verticalQuerySent',
+    'yext.answers.verticalQueryResponseReceived',
+    'yext.answers.verticalQueryResponseRendered'
+  ];
+  const searchComponent = FacetsPage.getSearchComponent();
+  await searchComponent.submitQuery();
+
+  // Wait for Results to be rendered
+  const resultsSelector = await Selector('.yxt-Results');
+  await resultsSelector.with({ visibilityCheck: true })();
+
+  // All performance marks should be called at least once with a search
+  for (let i = 0; i < marksToCheck.length; i++) {
+    const markName = marksToCheck[i];
+    const marksFoundWithName = await t.eval(() => {
+      return JSON.stringify(window.performance.getEntriesByName(markName));
+    }, { dependencies: { markName } });
+    await t.expect(JSON.parse(marksFoundWithName.length)).gt(0);
+  }
+});
