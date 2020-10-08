5db293e8548124d6c9e26ea48c52f629e195bb51
Oliver Shi <oshi@yext.com>
2020-10-08 10:32:28 -0400

    Add browserstack + testcafe integration with circleci (#1122)
    This commit updates our circle ci config to run our testcafe
    acceptance tests on browserstack. Videos can be viewed under
    the browserstack automate section of browserstack.
    
    TEST=auto
    
    See that in circle ci, and in browserstack automate, our acceptance
    tests are being run
    
    see that in browserstack automate, our tests have the Answers SDK project,
    each branch gets its own build tag, and each commit in a branch gets a test
    session under the build tab
    
    test that acceptance tests are run for build_and_deploy_i18n as well

---
+++ .circleci/run_acceptance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export BROWSERSTACK_USE_AUTOMATE="1"
+export BROWSERSTACK_PROJECT_NAME="Answers SDK"
+export BROWSERSTACK_BUILD_ID="${CIRCLE_BRANCH} - ${CIRCLE_BUILD_NUM}"
+COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
+export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
+
+# Browserstack only allows for 2 threads to run at once, so we have to break up the browsers
+npx testcafe "browserstack:firefox,browserstack:chrome" tests/acceptance/acceptancesuite.js
+npx testcafe "browserstack:safari,browserstack:ie@11.0" tests/acceptance/acceptancesuite.js
