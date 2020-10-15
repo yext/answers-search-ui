11462bbf6c0ed5c6ae8f5c62163da7dc06ab0f64
Oliver Shi <oshi@yext.com>
2020-10-15 15:08:05 -0400

    remove percy exec

--- .circleci/run_acceptance.sh
+++ .circleci/run_acceptance.sh
@@ -6,5 +6,5 @@ COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 
 # Browserstack only allows for 2 threads to run at once, so we have to break up the browsers
-npx percy exec -- npx testcafe "browserstack:firefox,browserstack:chrome" tests/acceptance/acceptancesuite.js
-npx percy exec -- npx testcafe "browserstack:safari,browserstack:ie@11.0" tests/acceptance/acceptancesuite.js
+npx testcafe "browserstack:firefox,browserstack:chrome" tests/acceptance/acceptancesuite.js
+npx testcafe "browserstack:safari,browserstack:ie@11.0" tests/acceptance/acceptancesuite.js
