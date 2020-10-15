e09d96eb7f2403183d39473b90b25750427d2912
Oliver Shi <oshi@yext.com>
2020-10-15 19:14:11 -0400

    install globally

--- .circleci/run_acceptance.sh
+++ .circleci/run_acceptance.sh
@@ -4,7 +4,8 @@ export BROWSERSTACK_PROJECT_NAME="Answers SDK"
 export BROWSERSTACK_BUILD_ID="${CIRCLE_BRANCH} - ${CIRCLE_BUILD_NUM}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
-
+npm install -g percy
+npm install -g testcafe
 # Browserstack only allows for 2 threads to run at once, so we have to break up the browsers
-npx percy exec -- npx testcafe "browserstack:chrome" tests/acceptance/acceptancesuite.js
+percy exec -- testcafe "browserstack:chrome" tests/acceptance/acceptancesuite.js
 # percy exec -- testcafe "browserstack:safari,browserstack:ie@11.0" tests/acceptance/acceptancesuite.js
