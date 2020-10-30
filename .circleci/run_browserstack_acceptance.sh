300906cf21ca32d5f9a92c78139d9dbfc63dd4b3
Oliver Shi <oshi@yext.com>
2020-10-30 15:18:18 -0400

    run safari and ie11 in parallel

--- .circleci/run_browserstack_acceptance.sh
+++ .circleci/run_browserstack_acceptance.sh
@@ -5,5 +5,4 @@ export BROWSERSTACK_BUILD_ID="${CIRCLE_BRANCH} - ${CIRCLE_BUILD_NUM}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 
-npx testcafe "browserstack:ie@11.0" tests/acceptance/acceptancesuite.js
-npx testcafe "safari" tests/acceptance/acceptancesuite.js
+npx testcafe "browserstack:ie@11.0,safari" tests/acceptance/acceptancesuite.js
