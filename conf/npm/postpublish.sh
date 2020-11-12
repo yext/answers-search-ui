52d3f3be02302f5b196555c60005ed90069d2ea0
creotutar <creotutar@yext.com>
2020-11-12 18:05:24 -0500

    Typo in postpublish

--- conf/npm/postpublish.sh
+++ conf/npm/postpublish.sh
@@ -1,4 +1,4 @@
-# This is after before a publish using `npm publish`
+# This is run after a publish using `npm publish`
 # We want to undo the changes done by the prepublish
 
 mv ./README.tmp ./README.md
