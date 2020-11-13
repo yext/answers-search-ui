1e3b912079f1dd7f46294683d323b05b0026d367
Tom Meyer <tmeyer@yext.com>
2020-11-13 16:01:52 -0500

    Correct bundle.js regexs.

--- conf/gulp-tasks/bundle/bundle.js
+++ conf/gulp-tasks/bundle/bundle.js
@@ -31,11 +31,11 @@ exports.modernBundle = function (callback, outputConfig, bundleName, locale, lib
     plugins: [
       resolve(),
       commonjs({
-        include: [/^(\.\/)?node_modules\/.*$/, /answers-core/]
+        include: [/node_modules/, /answers-core/]
       }),
       babel({
         babelrc: false,
-        exclude: [/^(\.\/)?node_modules\/.*$/, /answers-core/],
+        exclude: [/node_modules/, /answers-core/],
         presets: ['@babel/env']
       })
     ]
@@ -63,7 +63,7 @@ exports.legacyBundle = function (callback, outputConfig, bundleName, locale, lib
     plugins: [
       resolve(),
       commonjs({
-        include: [/^(\.\/)?node_modules\/.*$/, /answers-core/]
+        include: [/node_modules/, /answers-core/]
       }),
       babel({
         runtimeHelpers: true,
