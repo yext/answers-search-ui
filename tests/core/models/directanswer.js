248c324680ff77c956493b630999c681e06665b9
cea2aj <42848445+cea2aj@users.noreply.github.com>
2020-12-02 12:08:17 -0500

    Answers core adapter tests (#1196)
    Test the adapters from the answers core models the the SDK models
    
    J=SLAP-891
    Test=auto
    
    Run the tests and confirm they pass

---
+++ tests/core/models/directanswer.js
@@ -0,0 +1,47 @@
+import DirectAnswer from '../../../src/core/models/directanswer';
+
+it('constructs a direct answer from an answers-core direct answer', () => {
+  const coreRelatedResult = {
+    rawData: {
+      food_type: 'fruit'
+    },
+    id: 1,
+    type: 'ce_fruit',
+    link: 'yext.com'
+  };
+
+  const coreDirectAnswer = {
+    value: 'red',
+    relatedResult: coreRelatedResult,
+    verticalKey: 'foods',
+    entityName: 'apple',
+    fieldName: 'color',
+    fieldApiName: 'c_color',
+    fieldType: 'color'
+  };
+
+  const expectedDirectAnswer = {
+    answer: {
+      entityName: 'apple',
+      fieldName: 'color',
+      fieldApiName: 'c_color',
+      value: 'red',
+      fieldType: 'color'
+    },
+    relatedItem: {
+      data: {
+        fieldValues: {
+          food_type: 'fruit'
+        },
+        id: 1,
+        type: 'ce_fruit',
+        website: 'yext.com'
+      },
+      verticalConfigId: 'foods'
+    }
+  };
+
+  const actualDirectAnswer = DirectAnswer.fromCore(coreDirectAnswer);
+
+  expect(actualDirectAnswer).toMatchObject(expectedDirectAnswer);
+});
---
+++ tests/core/models/navigation.js
@@ -0,0 +1,17 @@
+import Navigation from '../../../src/core/models/navigation';
+
+it('constructs a navigation model from an answers-core vertical results array', () => {
+  const verticalResultsArray = [{ verticalKey: 'faqs' }, { verticalKey: 'locations' }, { verticalKey: 'products' }];
+
+  const expectedNavigation = {
+    tabOrder: [
+      'faqs',
+      'locations',
+      'products'
+    ]
+  };
+
+  const actualNavigation = Navigation.fromCore(verticalResultsArray);
+
+  expect(actualNavigation).toMatchObject(expectedNavigation);
+});
