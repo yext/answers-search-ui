8e965c451894bac72a1bd969791448cefe0e4d9b
Alexis Grow <agrow@yext.com>
2020-12-02 09:37:28 -0700

    missed a partial file staging

--- src/core/core-adapter.js
+++ src/core/core-adapter.js
@@ -219,7 +219,7 @@ export default class CoreAdapter {
         referrerPageUrl: referrerPageUrl,
         querySource: this.globalStorage.getState(StorageKeys.QUERY_SOURCE)
       })
-      .then(response => SearchDataTransformer.transformVertical(response, verticalKey))
+      .then(response => SearchDataTransformer.transformVertical(response, this._fieldFormatters, verticalKey))
       .then(data => {
         this.globalStorage.set(StorageKeys.QUERY_ID, data[StorageKeys.QUERY_ID]);
         this.globalStorage.set(StorageKeys.NAVIGATION, data[StorageKeys.NAVIGATION]);
