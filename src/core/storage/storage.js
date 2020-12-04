b526e89eb0456742ec14b628d12b9c806d1ea1af
creotutar <creotutar@yext.com>
2020-12-04 11:59:22 -0500

    Add newline bt jsdoc desc and param

--- src/core/storage/storage.js
+++ src/core/storage/storage.js
@@ -19,6 +19,7 @@ export default class GlobalStorage {
    * Decodes the initial state from the query params. This could be a
    * direct mapping from query param to storage keys in the storage or
    * could fetch a sessionId from some backend
+   *
    * @param {string} url The starting URL
    */
   init (url) {}
@@ -26,6 +27,7 @@ export default class GlobalStorage {
   /**
    * Set the data in storage with the given key to the provided
    * data, completely overwriting any existing data.
+   *
    * @param {string} key The storage key to set
    * @param {*} data The data to set
    */
@@ -34,6 +36,7 @@ export default class GlobalStorage {
   /**
    * Updates the storage and adds the entry to a persistent storage
    * buffer. The entry is not yet added to the persistent storage.
+   *
    * @param {string} key The storage key to set
    * @param {*} data The data to set
    */
@@ -47,6 +50,7 @@ export default class GlobalStorage {
 
   /**
    * Get the current state for the provided key
+   *
    * @param {string} key The storage key to get
    * @return {*} The state for the provided key
    */
@@ -54,6 +58,7 @@ export default class GlobalStorage {
 
   /**
    * Get the current state for all key/value pairs in storage
+   *
    * @return {Object} mapping from key to value representing the
    *                  current state
    */
@@ -62,12 +67,14 @@ export default class GlobalStorage {
   /**
    * Remove the data in storage with the given key to the
    * provided data
+   *
    * @param {string} key The storage key to delete
    */
   delete (key) {}
 
   /**
    * Returns the query parameters to encode the current state
+   *
    * @return {string} The query parameters for a page link with the
    *                  current state encoded
    *                  e.g. ?query=all&context=%7Bkey:'hello'%7D
@@ -76,6 +83,7 @@ export default class GlobalStorage {
 
   /**
    * Adds a listener to the given module for a given event
+   *
    * @param {string} event The event to listen to, e.g. ‘update’
    * @param {string} module The module key to listen for e.g. Pagination
    * @param {Function} callback The callback when an event is called,
