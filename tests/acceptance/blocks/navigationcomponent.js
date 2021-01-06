acf3ee31a547d91518fec6a98e0831e0523d4855
Connor Anderson <canderson@yext.com>
2021-01-05 20:20:03 -0500

    get tab links

--- tests/acceptance/blocks/navigationcomponent.js
+++ tests/acceptance/blocks/navigationcomponent.js
@@ -25,4 +25,22 @@ export default class NavigationComponentBlock {
 
     return tabLabels;
   }
+
+  /**
+   * Get an array of the links of the nav bar tabs
+   *
+   * @returns {Array<string>}
+   */
+  async getTabLinks () {
+    const tabList = this._navList.child();
+    const tabListCount = await tabList.count;
+    const tabLinks = [];
+
+    for (let i = 0; i < tabListCount; i++) {
+      const nthTab = tabList.nth(i);
+      tabLinks[i] = await nthTab.find('a').getAttribute('href');
+    }
+
+    return tabLinks;
+  }
 }
