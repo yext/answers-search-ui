33ffca89813a8d410cd9d3e12c1a011a5d4af3b9
Connor Anderson <canderson@yext.com>
2021-01-07 12:29:35 -0500

    remove unused code

--- tests/acceptance/utils.js
+++ tests/acceptance/utils.js
@@ -2,16 +2,6 @@ import { ClientFunction } from 'testcafe';
 
 /* global location */
 
-const getPageUrl = ClientFunction(() => window.location.href);
-
-export async function getURLSearchParams () {
-  const pageUrl = await getPageUrl();
-  const indexOfQuestionMark = pageUrl.indexOf('?');
-  const queryString = pageUrl.substring(indexOfQuestionMark);
-
-  return new URLSearchParams(queryString);
-}
-
 export async function browserRefreshPage () {
   await ClientFunction(() => location.reload())();
   await new Promise(resolve => setTimeout(resolve, 2500));
