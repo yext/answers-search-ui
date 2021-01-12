6e84bc53610e1efaea05324c1743bb79af949b8a
Connor Anderson <canderson@yext.com>
2021-01-12 16:58:49 -0500

    add tests for redirect links

---
+++ tests/ui/components/search/spellcheckcomponent.js
@@ -0,0 +1,66 @@
+import DOM from '../../../../src/ui/dom/dom';
+import mockManager from '../../../setup/managermocker';
+import StorageKeys from '../../../../src/core/storage/storagekeys';
+import SpellCheckComponent from '../../../../src/ui/components/search/spellcheckcomponent';
+
+const globalStorage = {};
+
+const mockCore = {
+  globalStorage: {
+    getState: key => globalStorage[key] || undefined,
+    set: (key, value) => {
+      globalStorage[key] = value;
+    }
+  }
+};
+
+const COMPONENT_MANAGER = mockManager(mockCore);
+
+describe('spellcheck redirect links', () => {
+  let linkUrlParams;
+
+  beforeEach(() => {
+    setupDOM();
+
+    const component = createSpellcheckComponent();
+
+    component.setState({
+      correctedQuery: {
+        value: 'What is Yext Answers?'
+      },
+      type: 'SUGGEST'
+    });
+
+    const correctedQueryUrl = component.getState('correctedQueryUrl');
+    linkUrlParams = new URLSearchParams(correctedQueryUrl);
+  });
+
+  it('redirect links contain a query url param', () => {
+    const query = linkUrlParams.get('query');
+    expect(query).toEqual('What is Yext Answers?');
+  });
+
+  it('redirect links contain a skipSpellCheck url param', () => {
+    const skipSpellCheck = linkUrlParams.get('skipSpellCheck');
+    expect(skipSpellCheck).toEqual('true');
+  });
+
+  it('redirect links contain a query trigger url param', () => {
+    const queryTrigger = linkUrlParams.get(StorageKeys.QUERY_TRIGGER);
+    expect(queryTrigger).toEqual('suggest');
+  });
+});
+
+function setupDOM () {
+  const bodyEl = DOM.query('body');
+  DOM.empty(bodyEl);
+  DOM.append(bodyEl, DOM.createEl('div', { id: 'spellcheck-component' }));
+}
+
+function createSpellcheckComponent () {
+  const defaultConfig = {
+    container: '#spellcheck-component',
+    verticalKey: 'verticalKey'
+  };
+  return COMPONENT_MANAGER.create(SpellCheckComponent.type, defaultConfig);
+}
