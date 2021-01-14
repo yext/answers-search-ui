7c687740cebaba1a58c653b0dc1808bb8602c071
Oliver Shi <oshi@yext.com>
2021-01-14 16:55:19 -0500

    cut over AUTOCOMPLETE to new global storage
    TEST=manual
    
    test that autocomplete still works for universal and vertical search
    
    test that I can search for a filter in filtersearch, apply it, and
    that the network tab shows a filter was applied to the query
    
    test that I can search for a filter in the geolocation filter,
    apply it, and that the network tab shows a filter was applied to the query

--- src/core/storage/storagekeys.js
+++ src/core/storage/storagekeys.js
@@ -11,7 +11,7 @@ export default {
   UNIVERSAL_RESULTS: 'universal-results',
   VERTICAL_RESULTS: 'vertical-results',
   ALTERNATIVE_VERTICALS: 'alternative-verticals',
-  AUTOCOMPLETE: 'autocomplete',
+  AUTOCOMPLETE: 'autocomplete', // Has been cut over to the new global storage
   DIRECT_ANSWER: 'direct-answer',
   FILTER: 'filter', // DEPRECATED
   STATIC_FILTER_NODE: 'static-filter-node',
