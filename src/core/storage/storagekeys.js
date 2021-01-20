fd3679f18d130856cc842b9e603543208e2526de
Oliver Shi <oshi@yext.com>
2021-01-20 15:17:17 -0500

    cut over API_CONTEXT to new global storage (#1269)
    TEST=manual,auto
    
    test that I can load up noresults, see that the
    universal link does nto have a context param, enter
    ANSWERS.setContext({'state': 'hi'}) into the console,
    then see that the universal link DOES have a context param

--- src/core/storage/storagekeys.js
+++ src/core/storage/storagekeys.js
@@ -32,7 +32,7 @@ export default {
   NO_RESULTS_CONFIG: 'no-results-config', // Has been cut over to the new global storage
   LOCATION_RADIUS: 'location-radius', // Has been cut over to the new global storage
   RESULTS_HEADER: 'results-header',
-  API_CONTEXT: 'context',
+  API_CONTEXT: 'context', // Has been cut over to the new global storage
   REFERRER_PAGE_URL: 'referrerPageUrl',
   QUERY_TRIGGER: 'queryTrigger',
   FACETS_LOADED: 'facets-loaded',
