6f28c09e40aab83f2ac54f46c481619ab8cd12c7
Oliver Shi <oshi@yext.com>
2021-01-20 15:22:29 -0500

    cut over SEARCH_CONFIG to new global storage (#1259)
    This commit also stops the SortOptions component
    from complaining about no verticalKey being present
    in the config, as long as you have one in the global
    SEARCH_CONFIG.
    
    J=SLAP-1019
    TEST=manual
    
    check that sort options, applied filters, location bias can use the global SEARCH_CONFIG.verticalKey
    check that pagination uses SEARCH_CONFIG.imit
    check that default initial search works

--- src/core/core.js
+++ src/core/core.js
@@ -174,9 +174,9 @@ export default class Core {
       }
     }
 
-    const searchConfig = this.globalStorage.getState(StorageKeys.SEARCH_CONFIG) || {};
+    const searchConfig = this.storage.get(StorageKeys.SEARCH_CONFIG) || {};
     if (!searchConfig.verticalKey) {
-      this.globalStorage.set(StorageKeys.SEARCH_CONFIG, {
+      this.storage.set(StorageKeys.SEARCH_CONFIG, {
         ...searchConfig,
         verticalKey: verticalKey
       });
@@ -188,7 +188,7 @@ export default class Core {
     );
     return this._searcher
       .verticalSearch(verticalKey, {
-        limit: this.globalStorage.getState(StorageKeys.SEARCH_CONFIG).limit,
+        limit: this.storage.get(StorageKeys.SEARCH_CONFIG).limit,
         geolocation: this.storage.get(StorageKeys.GEOLOCATION),
         ...parsedQuery,
         filter: this.filterRegistry.getStaticFilterPayload(),
