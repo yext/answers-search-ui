04d857845b00e1d0ad4f2424f01e4c187b9d61ea
Oliver Shi <oshi@yext.com>
2021-01-28 15:32:23 -0500

    use custom waiting logic

--- tests/acceptance/requestUtils.js
+++ tests/acceptance/requestUtils.js
@@ -8,7 +8,16 @@ import { t } from 'testcafe';
  * @returns {Promise<URLSearchParams>} the query params of the last request
  */
 export async function getMostRecentQueryParamsFromLogger (logger) {
-  await t.expect(logger.contains(record => record.response.statusCode === 200)).ok();
+  async function waitForFirstRequest () {
+    for (let i = 0; i < 50; i++) {
+      await t.wait(100);
+      if (logger.requests.length > 0) {
+        return;
+      }
+    }
+    throw new Error('No requests found for logger');
+  }
+  await waitForFirstRequest();
   const url = logger.requests[logger.requests.length - 1].request.url;
   return new URLSearchParams(url);
 }
