26aad52500019aad9b11cc3ff785a0bb1fb372d5
Oliver Shi <oshi@yext.com>
2021-02-18 16:50:32 -0500

    update jsdoc

--- src/ui/tools/filterutils.js
+++ src/ui/tools/filterutils.js
@@ -3,9 +3,9 @@ import isEqual from 'lodash.isequal';
 import Filter from '../../core/models/filter';
 
 /**
- * Checks whether the given filter is included or equal to
- * the filter persisted in the URL. Assumes the given filter
- * is a simple filter, i.e. does not have any child filters.
+ * Checks whether a filter is equal to or included somewhere within the persistedFilter.
+ * Assumes the given filter is a simple filter, i.e. does not have any child filters.
+ * The persistedFilter can be either combined or simple.
  *
  * @param {Filter} filter
  * @param {Filter} persistedFilter
