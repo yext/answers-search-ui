fa7a8808f08b1797852a82f1b83b0499b40ec143
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2021-06-24 09:10:59 -0400

    Update SDK to allow DA on vertical search (#1438)
    * Update SDK to allow DA on vertical search
    
    add direct answers to vertical search in sdk core data transformer layer
    Note: this sdk is using core version 1.3.0-alpha to get vertical search responses with direct answer
    
    J=SLAP-1389
    TEST=auto and manual
    
    Update and run jest tests to see that results are formatted properly.
    Verify direct answer is fetched properly from answers-core with console log.
    Launched theme test site using local sdk bundle, performed a vertical search on people's page and can verify that direct answer is present in network response
    
    Co-authored-by: Yen Truong <ytruong@yext.com>

--- src/ui/components/results/directanswercomponent.js
+++ src/ui/components/results/directanswercomponent.js
@@ -181,7 +181,7 @@ export default class DirectAnswerComponent extends Component {
       verticalKey: relatedItem.verticalConfigId,
       directAnswer: true,
       fieldName: this.getState('answer').fieldApiName,
-      searcher: 'UNIVERSAL',
+      searcher: this._config.isUniversal ? 'UNIVERSAL' : 'VERTICAL',
       entityId: relatedItem.data.id,
       url: event.target.href
     };
@@ -215,7 +215,7 @@ export default class DirectAnswerComponent extends Component {
     }
     return JSON.stringify({
       verticalConfigId: data.relatedItem.verticalConfigId,
-      searcher: 'UNIVERSAL',
+      searcher: this._config.isUniversal ? 'UNIVERSAL' : 'VERTICAL',
       entityId: data.relatedItem.data.id,
       ctaLabel: this._viewDetailsText.toUpperCase().replace(' ', '_')
     });
