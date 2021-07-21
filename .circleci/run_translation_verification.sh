2115d301202659c9cc1928763240e21712d84768
Connor Anderson <canderson@yext.com>
2021-07-21 11:11:14 -0400

    update script

--- .circleci/run_translation_verification.sh
+++ .circleci/run_translation_verification.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Verifiy that the messages.pot file in the repo is up to date
+# If there are any git diffs after translations are extracted, the checked-in file is out of date
+
 npm run extract-translations
+
+git diff --exit-code > /dev/null # send stdout to /dev/null to reduce clutter in the CI output
+diff_exit_code=$?
+
+if test $diff_exit_code -eq 1
+then
+  echo "Extracted translations are out of date. Run 'npm run extract-translations' and commit the updated pot file."
+  exit 1
+else
+  echo "Extracted translations are up to date"
+fi
 
 cd conf/i18n/translations
 
+# Verify that all translations are present
 for po_file in *.po
   do msgcmp $po_file messages.pot
 done
