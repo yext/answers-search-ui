1014e9c07624609b083d8f8d01de2a11a429d96f
Yen Truong <ytruong@yext.com>
2021-07-26 14:04:05 -0400

    update script

--- .circleci/run_browserstack_acceptance.sh
+++ .circleci/run_browserstack_acceptance.sh
@@ -4,14 +4,13 @@ export BROWSERSTACK_PROJECT_NAME="Answers SDK"
 export BROWSERSTACK_BUILD_ID="${CIRCLE_BRANCH} - ${CIRCLE_BUILD_NUM}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
-hardcodeBranch='hotfix/some-test'
 
-if [[ $hardcodeBranch == release/*
-  || $hardcodeBranch == hotfix/*
-  || $hardcodeBranch == master
-  || $hardcodeBranch == support* ]]
+if [[ $CIRCLE_BRANCH == release/*
+  || $CIRCLE_BRANCH == hotfix/*
+  || $CIRCLE_BRANCH == master
+  || $CIRCLE_BRANCH == support* ]]
 then
-  npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:safari browserstack:firefox browserstack:chrome
+  npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:safari
 else
-  npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:chrome --concurrency 2
+  npm run acceptance -- --browsers browserstack:ie@11.0 --concurrency 2
 fi
