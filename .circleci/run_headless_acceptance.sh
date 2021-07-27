a0c971fbdcf4299972f737ca1fedc3d4dd2cbb13
Yen Truong <ytruong@yext.com>
2021-07-27 11:47:00 -0400

    hardcode path

--- .circleci/run_headless_acceptance.sh
+++ .circleci/run_headless_acceptance.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-CONFIG_FILE=$(dirname $0)/testcafe.json
-
 if [[ $CIRCLE_BRANCH == release/*
   || $CIRCLE_BRANCH == hotfix/*
   || $CIRCLE_BRANCH == master
   || $CIRCLE_BRANCH == support* ]]
 then
-  npx testcafe -c 3 "chrome:headless,firefox:headless" --config-file $CONFIG_FILE
+  npx testcafe -c 3 "chrome:headless,firefox:headless" --config-file ./.circleci/testcafe.json
 else
-  npx testcafe -c 3 "chrome:headless" --config-file $CONFIG_FILE
+  npx testcafe -c 3 "chrome:headless" --config-file ./.circleci/testcafe.json
 fi
