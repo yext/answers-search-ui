7270dc7a4909dae13b8d1e417a36c84c8b2b0a14
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-07-28 12:40:55 -0400

    Update translation check (#1488)
    Update the translation check to correspond with the changes to the hitchhikers theme's check
    
    J=none
    TEST=manual
    
    Confirm that the test fails when the messages.pot file is out of date or if translations are missing

--- .circleci/run_headless_acceptance.sh
+++ .circleci/run_headless_acceptance.sh
@@ -3,7 +3,7 @@
 if [[ $CIRCLE_BRANCH == release/*
   || $CIRCLE_BRANCH == hotfix/*
   || $CIRCLE_BRANCH == master
-  || $CIRCLE_BRANCH == support* ]]
+  || $CIRCLE_BRANCH == support/* ]]
 then
   npx testcafe -c 3 "chrome:headless,firefox:headless" --config-file ./.circleci/testcafe.json
 else
