dea3a623987a2e0af37e6ad81f86e046f82423ee
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-07-30 16:29:02 -0400

    Add RTL CSS bundle (#1493)
    Add an RTL CSS bundle
    
    The RTL bundle does a few things including putting the filter checkboxes on the right side. We probably don't need to build the RTL bundle unless we are building for all languages or locales, however it is so quick that I think it makes sense to just build it during dev mode since it makes developing RTL styling much faster.
    
    J=SLAP-1465
    TEST=manual
    
    Build a site with the RTL styling and see the filter checkboxes on the right side

--- conf/gulp-tasks/library.gulpfile.js
+++ conf/gulp-tasks/library.gulpfile.js
@@ -2,6 +2,8 @@ const { series, parallel, src, dest, watch } = require('gulp');
 const path = require('path');
 const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
+const rtlcss = require('gulp-rtlcss');
+const rename = require('gulp-rename');
 sass.compiler = require('sass');
 
 const getLibraryVersion = require('./utils/libversion');
@@ -111,11 +113,36 @@ async function createBundleTaskFactory (locale) {
 }
 
 function compileCSS () {
+  return new Promise(resolve => {
+    return parallel(compileLtrCSS, compileRtlCSS)(resolve);
+  });
+}
+
+/**
+ * Compiles the standard left to right CSS
+ */
+function compileLtrCSS () {
   return src('./src/ui/sass/**/*.scss')
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(postcss())
+    .pipe(dest('./dist/'));
+}
+
+/**
+ * Compiles the right to left CSS which is used for languages such as Arabic
+ */
+function compileRtlCSS () {
+  return src('./src/ui/sass/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(rtlcss())
+    .pipe(postcss())
+    .pipe(rename(function (path) {
+      path.basename += '.rtl';
+    }))
     .pipe(dest('./dist/'));
 }
 
