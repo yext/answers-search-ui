e1a8655b7b1d0b1b8adc99df3b7145aa59d939e1
Oliver Shi <oshi@yext.com>
2021-08-17 16:51:05 -0400

    use edge from bstack

--- .circleci/run_useragent_acceptance.sh
+++ .circleci/run_useragent_acceptance.sh
@@ -8,4 +8,4 @@ export BROWSERSTACK_BUILD_ID="${CIRCLE_BRANCH} - ${CIRCLE_BUILD_NUM}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 
-npx testcafe chrome:headless,firefox:headless,browserstack:safari,edge tests/acceptance/useragent/useragentsuite.js --config-file ./.circleci/testcafe.json
+npx testcafe chrome:headless,firefox:headless,browserstack:safari,browserstack:edge tests/acceptance/useragent/useragentsuite.js --config-file ./.circleci/testcafe.json
