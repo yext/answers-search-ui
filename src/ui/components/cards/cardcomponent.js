07095967ebbc0d5a62ddf1d2b961a1a54ceaef06
Yen Truong <ytruong@yext.com>
2021-09-21 15:58:51 -0400

    configure through dataMapping; provide raw data with method

--- src/ui/components/cards/cardcomponent.js
+++ src/ui/components/cards/cardcomponent.js
@@ -113,13 +113,13 @@ export default class CardComponent extends Component {
 
     // Use the cardType as component name if it is not a built-in type
     const cardComponentName = cardTypes[cardType] || cardType;
-    const result = {
-      ...this.result,
-      distance: localizedDistance(this.result.distance, this.core.storage.get(StorageKeys.LOCALE))
-    };
+
+    // additional field(s) for dataMappings config option
+    this.result._raw.distance = this.result.distance;
+
     return super.setState({
       ...data,
-      result: result,
+      result: this.result,
       cardType: cardComponentName
     });
   }
