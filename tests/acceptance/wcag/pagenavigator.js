c8d7d6ca45202c06bcfd0a3a34eb12194fd3aad8
Yen Truong <ytruong@yext.com>
2021-10-19 16:03:31 -0400

    different input selector

--- tests/acceptance/wcag/pagenavigator.js
+++ tests/acceptance/wcag/pagenavigator.js
@@ -22,8 +22,7 @@ class PageNavigator {
    * @param {string} selector The CSS selector to click on
    */
   async click (selector) {
-    const elem = await this._page.$(selector);
-    await elem.evaluate(b => b.click());
+    await this._page.evaluate((selector) => document.querySelector(selector).click(), selector);
     await waitTillHTMLRendered(this._page);
   }
 }
--- tests/acceptance/wcag/wcagreporter.js
+++ tests/acceptance/wcag/wcagreporter.js
@@ -27,7 +27,7 @@ class WcagReporter {
     this.results.push(await this._analyzer.analyze());
     await this._pageNavigator.goto('filterbox', { query: 'sales' });
     this.results.push(await this._analyzer.analyze());
-    await this._pageNavigator.click('.js-yxt-FilterOptions-checkboxInput');
+    await this._pageNavigator.click('input[value="United States"]');
     this.results.push(await this._analyzer.analyze());
     await this._pageNavigator.goto('vertical');
     this.results.push(await this._analyzer.analyze());
