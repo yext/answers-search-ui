1b01f322f8dd1c6a9efcfdd30bcf6cad66d9fc4a
Oliver Shi <oshi@yext.com>
2022-01-27 12:14:33 -0500

    set geolocation in all tests

--- tests/acceptance/acceptancesuites/filterboxsuite.js
+++ tests/acceptance/acceptancesuites/filterboxsuite.js
@@ -28,7 +28,7 @@ test('single option filterbox works with back/forward navigation and page refres
   await t.addRequestHooks(radiusFilterLogger);
   await registerIE11NoCacheHook(t, VERTICAL_SEARCH_URL_REGEX);
   const searchComponent = FacetsPage.getSearchComponent();
-  await searchComponent.enterQuery(' ');
+  await searchComponent.enterQuery('all');
   await searchComponent.submitQuery();
   await SearchRequestLogger.waitOnSearchComplete(t);
 
@@ -64,93 +64,93 @@ test('single option filterbox works with back/forward navigation and page refres
   await expectRequestLocationRadiusToEql(radiusFilterLogger, 40233.6);
 });
 
-// test('multioption filterbox works with back/forward navigation and page refresh', async t => {
-//   const filterBoxLogger = RequestLogger({
-//     url: VERTICAL_SEARCH_URL_REGEX
-//   });
-//   await t.addRequestHooks(filterBoxLogger);
-//   await registerIE11NoCacheHook(t, VERTICAL_SEARCH_URL_REGEX);
-//   const martyFilter = {
-//     c_puppyPreference: {
-//       $eq: 'Marty'
-//     }
-//   };
-//   const frodoFilter = {
-//     c_puppyPreference: {
-//       $eq: 'Frodo'
-//     }
-//   };
-//   const filterBox = FacetsPage.getStaticFilterBox();
-//   const staticFilter = await filterBox.getFilterOptions('STATIC FILTERS');
-//   const filterTags = Selector('.yxt-ResultsHeader-removableFilterTag');
-//   const searchComponent = FacetsPage.getSearchComponent();
-//   await searchComponent.enterQuery('all');
-//   await searchComponent.submitQuery();
+test('multioption filterbox works with back/forward navigation and page refresh', async t => {
+  const filterBoxLogger = RequestLogger({
+    url: VERTICAL_SEARCH_URL_REGEX
+  });
+  await t.addRequestHooks(filterBoxLogger);
+  await registerIE11NoCacheHook(t, VERTICAL_SEARCH_URL_REGEX);
+  const martyFilter = {
+    c_puppyPreference: {
+      $eq: 'Marty'
+    }
+  };
+  const frodoFilter = {
+    c_puppyPreference: {
+      $eq: 'Frodo'
+    }
+  };
+  const filterBox = FacetsPage.getStaticFilterBox();
+  const staticFilter = await filterBox.getFilterOptions('STATIC FILTERS');
+  const filterTags = Selector('.yxt-ResultsHeader-removableFilterTag');
+  const searchComponent = FacetsPage.getSearchComponent();
+  await searchComponent.enterQuery('all');
+  await searchComponent.submitQuery();
 
-//   // Click the 'Marty' checkbox
-//   await staticFilter.toggleOption('Marty');
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
-//   await t.expect(filterTags.count).eql(1);
-//   filterBoxLogger.clear();
+  // Click the 'Marty' checkbox
+  await staticFilter.toggleOption('Marty');
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
+  await t.expect(filterTags.count).eql(1);
+  filterBoxLogger.clear();
 
-//   // Click the 'Frodo' checkbox
-//   await staticFilter.toggleOption('Frodo');
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(filterTags.count).eql(2);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql({
-//     $or: [martyFilter, frodoFilter]
-//   });
-//   filterBoxLogger.clear();
+  // Click the 'Frodo' checkbox
+  await staticFilter.toggleOption('Frodo');
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(filterTags.count).eql(2);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql({
+    $or: [martyFilter, frodoFilter]
+  });
+  filterBoxLogger.clear();
 
-//   // Hit the back button, see the 'Frodo' filter disappear
-//   await browserBackButton();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
-//   await t.expect(filterTags.count).eql(1);
-//   filterBoxLogger.clear();
+  // Hit the back button, see the 'Frodo' filter disappear
+  await browserBackButton();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
+  await t.expect(filterTags.count).eql(1);
+  filterBoxLogger.clear();
 
-//   // Hit the back button, see the 'Marty' filter disappear
-//   await browserBackButton();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await expectRequestDoesNotContainParam(filterBoxLogger, 'filters');
-//   await t.expect(filterTags.count).eql(0);
+  // Hit the back button, see the 'Marty' filter disappear
+  await browserBackButton();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await expectRequestDoesNotContainParam(filterBoxLogger, 'filters');
+  await t.expect(filterTags.count).eql(0);
 
-//   // Hit the forward button, see the 'Marty' filter applied again
-//   await browserForwardButton();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
-//   await t.expect(filterTags.count).eql(1);
-//   filterBoxLogger.clear();
+  // Hit the forward button, see the 'Marty' filter applied again
+  await browserForwardButton();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql(martyFilter);
+  await t.expect(filterTags.count).eql(1);
+  filterBoxLogger.clear();
 
-//   // Hit the forward button, see the 'Frodo' filter applied again
-//   await browserForwardButton();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(filterTags.count).eql(2);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql({
-//     $or: [martyFilter, frodoFilter]
-//   });
-//   filterBoxLogger.clear();
+  // Hit the forward button, see the 'Frodo' filter applied again
+  await browserForwardButton();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(filterTags.count).eql(2);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql({
+    $or: [martyFilter, frodoFilter]
+  });
+  filterBoxLogger.clear();
 
-//   // Refresh the page
-//   await browserRefreshPage();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(filterTags.count).eql(2);
-//   await t.expect(await getRequestFilters(filterBoxLogger)).eql({
-//     $or: [martyFilter, frodoFilter]
-//   });
-// });
+  // Refresh the page
+  await browserRefreshPage();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(filterTags.count).eql(2);
+  await t.expect(await getRequestFilters(filterBoxLogger)).eql({
+    $or: [martyFilter, frodoFilter]
+  });
+});
 
-// test('locationRadius of 0 is persisted', async t => {
-//   const filterBox = FacetsPage.getStaticFilterBox();
-//   const radiusFilter = await filterBox.getFilterOptions('DISTANCE');
-//   await radiusFilter.toggleOption('0 miles');
-//   const filterTags = Selector('.yxt-ResultsHeader-removableFilterTag');
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(filterTags.count).eql(1);
+test('locationRadius of 0 is persisted', async t => {
+  const filterBox = FacetsPage.getStaticFilterBox();
+  const radiusFilter = await filterBox.getFilterOptions('DISTANCE');
+  await radiusFilter.toggleOption('0 miles');
+  const filterTags = Selector('.yxt-ResultsHeader-removableFilterTag');
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(filterTags.count).eql(1);
 
-//   // Refresh the page
-//   await browserRefreshPage();
-//   await SearchRequestLogger.waitOnSearchComplete(t);
-//   await t.expect(filterTags.count).eql(1);
-// });
+  // Refresh the page
+  await browserRefreshPage();
+  await SearchRequestLogger.waitOnSearchComplete(t);
+  await t.expect(filterTags.count).eql(1);
+});
