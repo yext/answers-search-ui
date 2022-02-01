ebd8d1c9d82c76c0d10475437be295b1cc3ba831
Oliver Shi <oshi@yext.com>
2022-02-01 10:41:37 -0500

    mock useragent

--- tests/ui/components/search/searchcomponent.js
+++ tests/ui/components/search/searchcomponent.js
@@ -163,7 +163,9 @@ describe('SearchBar component', () => {
           getUserMedia: () => Promise.resolve({
             getTracks: () => ([{ stop: jest.fn() }])
           })
-        }
+        },
+        // taken from Microsoft Edge on desktop
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76'
       }));
     });
 
