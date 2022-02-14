4a6ddc025cc071b4b6d4229b12a8e37dc44aac5c
Yen Truong <ytruong@yext.com>
2022-02-14 15:14:00 -0500

    remove onReject handler that is already handle in core.js

--- src/ui/components/search/searchcomponent.js
+++ src/ui/components/search/searchcomponent.js
@@ -579,10 +579,7 @@ export default class SearchComponent extends Component {
   promptForLocation (query) {
     if (this._promptForLocation) {
       return this.fetchQueryIntents(query)
-        .then(
-          queryIntents => queryIntents.includes('NEAR_ME'),
-          error => Promise.reject(new Error('Failed to fetch query intents.', { cause: error }))
-        )
+        .then(queryIntents => queryIntents.includes('NEAR_ME'))
         .then(queryHasNearMeIntent => {
           if (queryHasNearMeIntent && !this.core.storage.get(StorageKeys.GEOLOCATION)) {
             return new Promise((resolve, reject) =>
