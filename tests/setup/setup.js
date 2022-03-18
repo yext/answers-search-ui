35016bd1e538f9f98525e85ae0f05ac22824dd47
Oliver Shi <oshi@yext.com>
2022-03-18 15:09:40 -0400

    eslint and update regenerator runtime usage

--- tests/setup/setup.js
+++ tests/setup/setup.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
 import AnswersAdapter from './enzymeadapter';
-import 'regenerator-runtime/runtime';
-
+import regeneratorRuntime from 'regenerator-runtime/runtime';
+global.regeneratorRuntime = regeneratorRuntime;
 configure({ adapter: new AnswersAdapter() });
