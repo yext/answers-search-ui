b6dcc91787e6cffcd6d6d224e69bbbd76e640107
cea2aj <42848445+cea2aj@users.noreply.github.com>
2022-04-05 10:10:48 -0400

    Github Action Migration (#1700)
    Delete the Circle CI config and migrate to Github Actions
    
    The original gh-action-migration feature branch was based on develop, and we don't want to merge the extra commits from develop into master just yet. For this release, we only want the github action migration. Once this is merged into master, we can merge master into develop.
    
    J=SLAP-2023
    TEST=none
    
    The functionality was tested in the individual items.

---
+++ .github/validate_third_party_notices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script verifies that the THIRD-PARTY-NOTICES file is up-to-date
+
+npm run generate-notices -- --output THIRD-PARTY-NOTICES-CURRENT 
+
+diff THIRD-PARTY-NOTICES THIRD-PARTY-NOTICES-CURRENT > /dev/null # send stdout to /dev/null to reduce clutter in the CI output
+diff_exit_code=$?
+
+rm THIRD-PARTY-NOTICES-CURRENT
+
+if test $diff_exit_code -eq 1
+then
+  echo "The THIRD-PARTY-NOTICES files is out of date. Run 'npm run generate-notices' and commit the updated docs."
+  echo "You may need to run 'npm install' before generating the notices."
+  exit 1
+else
+  echo "The THIRD-PARTY-NOTICES file is up to date."
+fi
