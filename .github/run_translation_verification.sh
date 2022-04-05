c83d0e41f8a061c0eca360c9c0771cef5cdf25d0
cea2aj <42848445+cea2aj@users.noreply.github.com>
2022-04-05 10:10:48 -0400

    Github Action Migration (#1700)
    Delete the Circle CI config and migrate to Github Actions
    
    The original gh-action-migration feature branch was based on develop, and we don't want to merge the extra commits from develop into master just yet. For this release, we only want the github action migration. Once this is merged into master, we can merge master into develop.
    
    J=SLAP-2023
    TEST=none
    
    The functionality was tested in the individual items.

--- .github/run_translation_verification.sh
+++ .github/run_translation_verification.sh
@@ -16,14 +16,19 @@ fi
 
 # Verify that translations are present for all languages
 cd conf/i18n/translations
-if [[ $CIRCLE_BRANCH == release/*
-  || $CIRCLE_BRANCH == hotfix/*
-  || $CIRCLE_BRANCH == master
-  || $CIRCLE_BRANCH == support/* ]]
+
+exit_code=0
+if [[ $GITHUB_REF_NAME == release/*
+  || $GITHUB_REF_NAME == hotfix/*
+  || $GITHUB_REF_NAME == master
+  || $GITHUB_REF_NAME == support/* ]]
 then
   for po_file in *.po
-    do msgcmp $po_file messages.pot
+  do
+    msgcmp $po_file messages.pot || exit_code=1
   done
 else
   echo "Skipping the verification that all translations are present"
 fi
+
+exit $exit_code
