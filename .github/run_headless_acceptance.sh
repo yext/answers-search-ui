9ebf280ed6c0d2dce159eb85c509475b5b6be38f
Connor Anderson <canderson@yext.com>
2022-04-08 12:41:52 -0400

    Remove testcafe concurrency

--- .github/run_headless_acceptance.sh
+++ .github/run_headless_acceptance.sh
@@ -5,7 +5,7 @@ if [[ $GITHUB_REF_NAME == release/*
   || $GITHUB_REF_NAME == master
   || $GITHUB_REF_NAME == support/* ]]
 then
-  npx testcafe -c 3 "chrome:headless,firefox:headless" tests/acceptance/acceptancesuites/*.js -q
+  npx testcafe "chrome:headless,firefox:headless" tests/acceptance/acceptancesuites/*.js -q
 else
-  npx testcafe -c 3 "chrome:headless" tests/acceptance/acceptancesuites/*.js -q
+  npx testcafe "chrome:headless" tests/acceptance/acceptancesuites/*.js -q
 fi
