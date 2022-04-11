d0ee80c8e4f504a91b46774c249c06d21395aa46
Oliver Shi <oshi@yext.com>
2022-04-11 15:37:37 -0400

    fetchdepth 0

--- conf/gulp-tasks/utils/libversion.js
+++ conf/gulp-tasks/utils/libversion.js
@@ -6,9 +6,6 @@ const { execSync } = require('child_process');
  * @returns {string} The SDK asset's library version.
  */
 function getLibVersion () {
-  console.log(execSync('git --version').toString().trim());
-  console.log('tags---', execSync('git tag').toString().trim());
-  console.log('logs---', execSync('git log').toString().trim());
   try {
     const insideWorkTree =
       execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
