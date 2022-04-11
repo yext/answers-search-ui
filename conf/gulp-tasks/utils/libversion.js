691a39aee26300af0b046e0675c755af4aad5e7b
Oliver Shi <oshi@yext.com>
2022-04-11 15:09:42 -0400

    print git ver

--- conf/gulp-tasks/utils/libversion.js
+++ conf/gulp-tasks/utils/libversion.js
@@ -1,13 +1,16 @@
+const { execSync } = require('child_process');
+
 /**
  * Attempts to compute the correct library version for an SDK asset.
  *
  * @returns {string} The SDK asset's library version.
  */
 function getLibVersion () {
+  console.log(execSync('git --version').toString().trim());
   try {
-    const insideWorkTree = require('child_process')
-      .execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
-      .toString().trim();
+    const insideWorkTree =
+      execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
+        .toString().trim();
     if (insideWorkTree === 'true') {
       return require('child_process')
         .execSync('git describe --tags')
