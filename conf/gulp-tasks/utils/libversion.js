be6884e1a675e7d5652c70ff021b293bebbab540
Oliver Shi <oshi@yext.com>
2022-04-11 17:59:28 -0400

    add fetch-depth: 0 to actions/checkout usages (#1716)
    By default, actions/checkout only fetches the latest commit of the git tree in question.
    To fetch the full tree, we need to specify `fetch-depth: 0`. This is only necessary in the SDK, which
    bases its version off of git tag instead of package.json version. This was added only to actions/checkout
    usages that actually run the SDK's build, not those that just download the built output, or don't use the build
    output at all.
    
    We can't switch the SDK to use the package.json version, unless we want to drop the git hash in the version
    for canary/latest.
    
    https://github.com/actions/checkout#fetch-all-history-for-all-tags-and-branches
    
    I also updated `getLibVersion` to not quietly suppress errors, because that could lead to scenarios
    like this again. There's never really a time we would need to gracefully handle not being able to get
    the lib version correctly - those times are almost always bugs we need to catch.
    
    Additionally, I updated our github actions to all use node 16. We have package-lock v2, so it's detrimental to stay on 14.
    I also added `retryTestPages` to the testcafe config, which will automatically retry loading the page when the network
    request fails to load it. This change required adding `hostname: "localhost"` as well, since under the hood testcafe
    uses webworkers for this.
    
    J=none
    TEST=manual
    
    see that the assets for this branch set jsLibVersion and Client-SDK correctly

--- conf/gulp-tasks/utils/libversion.js
+++ conf/gulp-tasks/utils/libversion.js
@@ -1,3 +1,5 @@
+const { execSync } = require('child_process');
+
 /**
  * Attempts to compute the correct library version for an SDK asset.
  *
@@ -5,20 +7,18 @@
  */
 function getLibVersion () {
   try {
-    const insideWorkTree = require('child_process')
-      .execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
-      .toString().trim();
+    const insideWorkTree =
+      execSync('git rev-parse --is-inside-work-tree 2>/dev/null')
+        .toString().trim();
     if (insideWorkTree === 'true') {
       return require('child_process')
         .execSync('git describe --tags')
         .toString().trim();
     }
   } catch (e) {
-    // if above command fails, catch error and continue, as we are not in a git repository
+    console.error('Error getting lib version');
+    throw e;
   }
-
-  console.warn('Warning: Not in a github repository, using default hardcoded library version.');
-  return 'TEST';
 }
 
 module.exports = getLibVersion;
