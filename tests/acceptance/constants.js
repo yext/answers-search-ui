b5f4b90c3bc72b92d80f80c12bac52bf513e6a71
Oliver Shi <oshi@yext.com>
2022-06-21 14:42:22 -0400

    only run acceptancesuites.js, add .html suffix

--- tests/acceptance/constants.js
+++ tests/acceptance/constants.js
@@ -4,10 +4,10 @@ export const PORT = 9999;
 
 const PAGE_DIR = `http://localhost:${PORT}/tests/acceptance/fixtures/html`;
 
-export const UNIVERSAL_PAGE = `${PAGE_DIR}/universal`;
-export const VERTICAL_PAGE = `${PAGE_DIR}/vertical`;
-export const FACETS_PAGE = `${PAGE_DIR}/facets`;
-export const FACETS_ON_LOAD_PAGE = `${PAGE_DIR}/facetsonload`;
-export const FILTERBOX_PAGE = `${PAGE_DIR}/filterbox`;
-export const UNIVERSAL_INITIAL_SEARCH_PAGE = `${PAGE_DIR}/universalinitialsearch`;
-export const SEARCH_BAR_ONLY_PAGE = `${PAGE_DIR}/searchbaronly`;
+export const UNIVERSAL_PAGE = `${PAGE_DIR}/universal.html`;
+export const VERTICAL_PAGE = `${PAGE_DIR}/vertical.html`;
+export const FACETS_PAGE = `${PAGE_DIR}/facets.html`;
+export const FACETS_ON_LOAD_PAGE = `${PAGE_DIR}/facetsonload.html`;
+export const FILTERBOX_PAGE = `${PAGE_DIR}/filterbox.html`;
+export const UNIVERSAL_INITIAL_SEARCH_PAGE = `${PAGE_DIR}/universalinitialsearch.html`;
+export const SEARCH_BAR_ONLY_PAGE = `${PAGE_DIR}/searchbaronly.html`;
