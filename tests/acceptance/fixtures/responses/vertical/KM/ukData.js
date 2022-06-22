8b0679a5f307dec8fabc772b2485f2455ebde997
Oliver Shi <oshi@yext.com>
2022-06-22 11:07:08 -0400

    refactor vertical search acceptance fixtures into multiple files
    Refactors the vertical search acceptance fixtures into multiple files
    instead of sticking them all inside of one.
    
    The facets fixtures are more complicated and stuffing them into one file
    would be untenable.
    
    J=SLAP-2151
    TEST=none

---
+++ tests/acceptance/fixtures/responses/vertical/KM/ukData.js
@@ -0,0 +1,93 @@
+export const ukData = {
+  data: {
+    id: '7941089580646240971',
+    type: 'location',
+    website: 'https://locations.yext.com/gb/gt-lon/london/48-warwick-st-.html',
+    address: {
+      line1: '48 Warwick St.',
+      line2: 'Office 410',
+      city: 'London',
+      region: 'Gt Lon',
+      postalCode: 'W1B 5AW',
+      countryCode: 'GB'
+    },
+    addressHidden: false,
+    description: 'Yext is the global digital knowledge management leader, and our Knowledge Engine puts businesses in control of their digital knowledge.',
+    hours: {
+      monday: {
+        isClosed: true
+      },
+      tuesday: {
+        openIntervals: [
+          {
+            start: '15:00',
+            end: '16:00'
+          }
+        ]
+      },
+      wednesday: {
+        isClosed: true
+      },
+      thursday: {
+        isClosed: true
+      },
+      friday: {
+        isClosed: true
+      },
+      saturday: {
+        isClosed: true
+      },
+      sunday: {
+        isClosed: true
+      }
+    },
+    name: 'UK Office Space',
+    cityCoordinate: {
+      latitude: 51.50642013549805,
+      longitude: -0.12721000611782074
+    },
+    c_features: [
+      'Open Now'
+    ],
+    displayCoordinate: {
+      latitude: 51.5107139,
+      longitude: -0.1377914
+    },
+    geocodedCoordinate: {
+      latitude: 51.5106898,
+      longitude: -0.1378107
+    },
+    isoRegionCode: 'LND',
+    mainPhone: '+442037052290',
+    priceRange: '$$$$',
+    routableCoordinate: {
+      latitude: 51.51076,
+      longitude: -0.13767
+    },
+    services: [
+      'Dogs',
+      'Cats',
+      'Sleep'
+    ],
+    timezone: 'Europe/London',
+    websiteUrl: {
+      url: 'https://locations.yext.com/gb/gt-lon/london/48-warwick-st-.html',
+      preferDisplayUrl: false
+    },
+    yextDisplayCoordinate: {
+      latitude: 51.5107139,
+      longitude: -0.1377914
+    },
+    yextRoutableCoordinate: {
+      latitude: 51.51076,
+      longitude: -0.13767
+    },
+    categoryIds: [
+      '668'
+    ],
+    timeZoneUtcOffset: '+01:00',
+    uid: '18714999'
+  },
+  highlightedFields: {},
+  distance: 5900224
+};
---
+++ tests/acceptance/fixtures/responses/vertical/KM/vaData.js
@@ -0,0 +1,119 @@
+export const vaData = {
+  data: {
+    id: '2917513710699998040',
+    type: 'location',
+    website: 'https://locations.yext.com/us/va/mclean/7900-westpark-drive.html',
+    address: {
+      line1: '7900 Westpark Drive',
+      line2: 'Suite T200',
+      city: 'McLean',
+      region: 'VA',
+      postalCode: '22102',
+      countryCode: 'US'
+    },
+    addressHidden: false,
+    description: 'Yext is the global digital knowledge management leader, and our Knowledge Engine puts businesses in control of their digital knowledge.',
+    hours: {
+      monday: {
+        openIntervals: [
+          {
+            start: '01:00',
+            end: '14:00'
+          }
+        ]
+      },
+      tuesday: {
+        openIntervals: [
+          {
+            start: '02:00',
+            end: '15:00'
+          },
+          {
+            start: '16:00',
+            end: '17:00'
+          }
+        ]
+      },
+      wednesday: {
+        openIntervals: [
+          {
+            start: '00:00',
+            end: '23:59'
+          }
+        ]
+      },
+      thursday: {
+        isClosed: true
+      },
+      friday: {
+        openIntervals: [
+          {
+            start: '12:00',
+            end: '14:00'
+          }
+        ]
+      },
+      saturday: {
+        openIntervals: [
+          {
+            start: '12:00',
+            end: '13:00'
+          }
+        ]
+      },
+      sunday: {
+        isClosed: true
+      }
+    },
+    name: 'VA Office Space',
+    cityCoordinate: {
+      latitude: 38.936519622802734,
+      longitude: -77.18428039550781
+    },
+    c_names: [
+      'my name is Steve',
+      'your name is Rose'
+    ],
+    displayCoordinate: {
+      latitude: 38.9246498,
+      longitude: -77.2169181
+    },
+    facebookPageUrl: 'https://www.facebook.com/Yext-Cafe-2073644659328705/',
+    geocodedCoordinate: {
+      latitude: 38.9246498,
+      longitude: -77.2169181
+    },
+    isoRegionCode: 'VA',
+    localPhone: '+16467624579',
+    mainPhone: '+18884442988',
+    routableCoordinate: {
+      latitude: 38.9243984,
+      longitude: -77.2178386
+    },
+    services: [
+      'Dogs',
+      'Cats',
+      'Sleep'
+    ],
+    timezone: 'America/New_York',
+    websiteUrl: {
+      url: 'https://locations.yext.com/us/va/mclean/7900-westpark-drive.html',
+      preferDisplayUrl: false
+    },
+    yextDisplayCoordinate: {
+      latitude: 38.9246498,
+      longitude: -77.2169181
+    },
+    yextRoutableCoordinate: {
+      latitude: 38.9243984,
+      longitude: -77.2178386
+    },
+    categoryIds: [
+      '668'
+    ],
+    timeZoneUtcOffset: '-04:00',
+    uid: '18714998'
+  },
+  highlightedFields: {},
+  distance: 13128
+};
