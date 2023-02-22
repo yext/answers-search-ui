3ab4bcad93528b704ef53a6d0b7938cd4950579d
Oliver Shi <oshi@yext.com>
2023-02-22 12:34:53 -0500

    Remove Browserstack (#1806)
    Removes Browserstack acceptance test runners from the SDK. We still run the same acceptance tests
    in headless mode.
    
    J=none
    TEST=auto

--- .github/run_browserstack_acceptance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export BROWSERSTACK_USE_AUTOMATE="1"
-export BROWSERSTACK_PROJECT_NAME="Answers SDK"
-export BROWSERSTACK_BUILD_ID="${GITHUB_REF_NAME} - ${GITHUB_RUN_ID}"
-COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
-export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
-
-npx testcafe "browserstack:safari" --config-file ./.github/testcafe.json -q successThreshold=1
