a2be6b4632550061115cf19b565f3a68d2adf6a2
Connor Anderson <canderson@yext.com>
2023-05-05 13:46:12 -0400

    Pass CloudRegion to Search Core
    This change uses the CloudRegion injected during the build
    process and passes it to the Search Core config. It also
    passes the environment using the environment provided from
    the config rather than manually constructing the endpoints.
    
    J=BACK-2269
    TEST=manual
    
    Manually tested the eu assets with an eu test site
    and confirmed the requests were being made correctly. Also
    tested the search-bar only integration in eu. Confirmed the
    us build still worked as expected.

--- conf/cloud-regions/constants.js
+++ conf/cloud-regions/constants.js
@@ -1,10 +1,3 @@
-const CLOUD_REGION = {
-  US: 'us',
-  EU: 'eu'
-};
+const { CloudRegion } = require('@yext/search-core');
 
-exports.CLOUD_REGION = CLOUD_REGION;
-
-exports.DEFAULT_CLOUD_REGION = CLOUD_REGION.US;
-
-exports.ALL_CLOUD_REGIONS = Object.values(CLOUD_REGION);
+exports.DEFAULT_CLOUD_REGION = CloudRegion.US;
