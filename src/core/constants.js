3bc62d0a7f71a114f6c69bef2b91f58f12c386b3
Jacob Fondriest <jfondriest@yext.com>
2024-09-13 17:16:27 -0400

    update constants

--- src/core/constants.js
+++ src/core/constants.js
@@ -11,10 +11,10 @@ export const SPEECH_RECOGNITION_LOCALES_SUPPORTED_BY_EDGE = '@@SPEECH_RECOGNITIO
 
 /** The cloud region being used, injected by the build process */
 export const CLOUD_REGION = '@@CLOUD_REGION';
-/** The identifier of the production environment */
+/** The identifier for all cloud providers */
 export const GLOBAL_MULTI = 'multi';
 
-/** The identifier of the sandbox environment */
+/** The identifier for using GCP */
 export const GLOBAL_GCP = 'gcp';
 
 /** The identifier of the production environment */
