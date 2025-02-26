bb47c24e8951c1446db98b78d2ccdde5fdc3e1af
Jacob Fondriest <jfondriest@yext.com>
2025-02-26 17:24:57 -0500

    Adjust vertical test to check autocomplete

--- tests/acceptance/fixtures/responses/vertical/autocomplete.js
+++ tests/acceptance/fixtures/responses/vertical/autocomplete.js
@@ -3,8 +3,11 @@ import { CORSHeaders } from '../cors';
 import { meta } from './sharedData';
 
 function generateAutoCompleteResponse (prompt) {
-  return {
-    meta,
+  const mockedResponse = {
+    meta: {
+      uuid: '01802d71-9901-1b83-9d50-ff143088f1ab',
+      errors: []
+    },
     response: {
       input: {
         value: prompt,
@@ -13,11 +16,33 @@ function generateAutoCompleteResponse (prompt) {
       results: []
     }
   };
+
+  if (prompt === '') {
+    mockedResponse.response.results = [
+      {
+        value: 'a Rose by any other name',
+        matchedSubstrings: [],
+        queryIntents: [],
+        verticalKeys: []
+      }
+    ];
+  } else if (prompt.startsWith('vir')) {
+    mockedResponse.response.results = [
+      {
+        value: 'virginia',
+        matchedSubstrings: [],
+        queryIntents: [],
+        verticalKeys: []
+      }
+    ];
+  }
+
+  return mockedResponse;
 }
 
 export const MockedVerticalAutoCompleteRequest = RequestMock()
   .onRequestTo(async request => {
-    const urlRegex = /.*\.com\/v2\/accounts\/me\/search\/autocomplete/;
+    const urlRegex = /.*\.com\/v2\/accounts\/me\/search\/vertical\/autocomplete/;
     return urlRegex.test(request.url) && request.method === 'get';
   })
   .respond((req, res) => {
