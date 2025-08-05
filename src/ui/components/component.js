7a0346526db69920e55333e083dc48a2f02b23b2
Fondryext <160865254+Fondryext@users.noreply.github.com>
2025-08-05 16:36:40 -0400

    v1.18.6 (#1944)
    **ksearch: update click event listeners**
    This PR adjusts the event listeners from mousedown
    to click and auxclick, to better handle the common
    click events today.
    J=TECHOPS-14689

--- src/ui/components/component.js
+++ src/ui/components/component.js
@@ -494,14 +494,20 @@ export default class Component {
     const label = dataset.eventlabel;
     const middleclick = dataset.middleclick;
     const options = dataset.eventoptions ? JSON.parse(dataset.eventoptions) : {};
-
-    DOM.on(domComponent, 'mousedown', e => {
+    const handleClick = (e) => {
       if (e.button === 0 || (middleclick && e.button === 1)) {
         const event = new AnalyticsEvent(type, label);
         event.addOptions(this._analyticsOptions);
         event.addOptions(options);
         this.analyticsReporter.report(event);
       }
+    };
+
+    DOM.on(domComponent, 'click', e => {
+      handleClick(e);
+    });
+    DOM.on(domComponent, 'auxclick', e => {
+      handleClick(e);
     });
   }
 
